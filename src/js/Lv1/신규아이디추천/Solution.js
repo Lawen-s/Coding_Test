08e30ee2d1851f75d01490d1ed34ff3e61c78b34
Lawen-s <bg920611@gmail.com>
2021-12-27 00:24:20 +0900

    Lv1 신규아이디추천

--- src/js/Lv1/신규아이디추천/Solution.js
+++ src/js/Lv1/신규아이디추천/Solution.js
@@ -1,3 +1,4 @@
+//my
 function solution(new_id) {
     var answer = [];
     let changeId;
@@ -30,20 +31,73 @@ function solution(new_id) {
     } else {
         if (newArr.length >= 16) {
             newArr = newArr.slice(0, 15);
+            newArr = newArr.filter((el, idx) => !(idx === 0 && el === `.` || idx === newArr.length - 1 && el === `.`));
+            answer = newArr.join('');
         }
-        newArr = newArr.filter((el, idx) => !(idx === 0 && el === `.` || idx === newArr.length - 1 && el === `.`));
-        answer = newArr.join('');
         return answer;
     }
 }
-/*{
-    1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
-2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
-3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
-4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
-5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
-6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
-     만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
-7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
-}*/
-console.log(solution("z-+.^."))
+
+//Other1
+// function solution(new_id) {
+//     var answer = '';
+    
+//     new_id = new_id.toLowerCase();  // 1. 소문자로 치환
+//     new_id = new_id.replace(/[^a-z0-9-._]/gi, ''); // 2. 특정문자 외 제거
+
+//     // 3단계 : 연속된 . 하나로 치환
+//     var str = '';
+//     for(var i=0; i<new_id.length; i++){
+//         if(new_id[i] == '.' && new_id[i-1] == '.'){
+//             str += '';
+//         }else{
+//             str += new_id[i];
+//         }    
+//     }
+//     new_id = str;
+   
+//     // 4단계 : . 처음 또는 끝일 경우 제거
+//     new_id = four(new_id);
+    
+//     // 5단계
+//     if(new_id == '') new_id = 'a';
+   
+//     // 6단계
+//     if(new_id.length>15) new_id = new_id.substring(0,15);
+//     new_id = four(new_id);
+        
+//     // 7단계
+//     new_id = seven(new_id);
+
+//     answer = new_id;
+//     return answer;
+// }
+
+// function four(new_id){
+//     if(new_id.substring(0,1) == '.') new_id = new_id.slice(1);
+//     if(new_id.substring(new_id.length-1) == '.') new_id = new_id.substring(0,new_id.length-1);
+    
+//     return new_id;
+// }
+
+// function seven(new_id){
+//     if(new_id.length < 3){
+//         new_id = new_id + new_id.substring(new_id.length-1);
+//         return seven(new_id);
+//     }else{
+//         return new_id;
+//     }
+// }
+
+//other2
+// function solution(new_id) {    
+//     const answer = new_id
+//         .toLowerCase()
+//         .replace(/[^\w-_.]/g, '')
+//         .replace(/\.{2,}/g, '.')
+//         .replace(/^\.|\.$/g, '')
+//         .replace(/^$/, 'a')
+//         .slice(0, 15).replace(/\.$/, '');
+//     const len = answer.length;
+//     return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len);
+// }
