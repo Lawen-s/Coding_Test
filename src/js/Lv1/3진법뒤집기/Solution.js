d331a4770b557b86595358740c397861c5e92a41
Lawen-s <bg920611@gmail.com>
2022-01-07 22:24:06 +0900

    Lv1 3진법뒤집기

--- src/js/Lv1/3진법뒤집기/Solution.js
+++ src/js/Lv1/3진법뒤집기/Solution.js
@@ -3,5 +3,15 @@ function solution(n) {
     let reverseNum = n.toString(3).split('').reverse().join("");
     return parseInt(reverseNum, 3);
 }
+//내부함수 이용하지 않은 방법
+// function solution(n) {
+//     const answer = [];
+//     while (n !== 0) {
+//         answer.unshift(n % 3);
+//         n = Math.floor(n / 3);
+//     }
+//     console.log(answer);
+//     return answer.reduce((acc, v, i) => acc + (v * Math.pow(3, i)), 0);
+// }
 
 console.log(solution(45))
