c2d5aad29c2d8b8501caa0cfc8c65b2fd67f24e9
Lawen-s <bg920611@gmail.com>
2022-01-13 00:51:35 +0900

    Lv1 부족한금액계산하기

---
+++ src/js/Lv1/부족한금액계산하기/Solution.js
@@ -0,0 +1,11 @@
+function solution(price, money, count) {
+    let answer = 0;
+    for (let i = 1; i <= count; i++) {
+        answer += price * i;
+    }
+
+    return answer - money > 0 ? answer - money : 0;
+}
+
+
+console.log(solution(3, 20, 4))
