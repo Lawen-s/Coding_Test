0c5bbbc231e0cf705cc455e179ce72b870c6ef0f
Lawen-s <bg920611@gmail.com>
2022-01-17 00:03:18 +0900

    Lv1 [1차]다트게임

--- src/js/Lv1/[1차]다트게임/Solution.js
+++ src/js/Lv1/[1차]다트게임/Solution.js
@@ -1,30 +1,35 @@
 function solution(dartResult) {
     let answer = 0;
+    let score = 0;
     let newArr = [];
-    dartResult = dartResult.split('');
-    console.log(dartResult)
     for (let i = 0; i < dartResult.length; i++) {
+        if (dartResult[i] >= 0 && dartResult[i] <= 9) {
+            if (dartResult[i] === "1" && dartResult[i + 1] === "0") {
+                score = 10;
+                i++;
+            } else {
+                console.log(dartResult[i])
+                score = dartResult[i];
+            }
+        }
         if (dartResult[i] === `S`) {
-            console.log(dartResult[i - 1]);
-            newArr.push(dartResult[i - 1] * 1);
+            newArr.push(score * 1);
         } else if (dartResult[i] === 'D') {
-            newArr.push(dartResult[i - 1] * dartResult[i - 1]);
+            newArr.push(score * score);
         } else if (dartResult[i] === 'T') {
-            newArr.push(dartResult[i - 1] * dartResult[i - 1] * dartResult[i - 1]);
-        } else if (dartResult[i] === `*` || dartResult[i] === `#`) {
-            newArr.push(dartResult[i]);
+            newArr.push(score * score * score);
+        } else if (dartResult[i] === "*") {
+            newArr[newArr.length - 2] = newArr[newArr.length - 2] * 2;
+            newArr[newArr.length - 1] = newArr[newArr.length - 1] * 2;
+        }
+        else if (dartResult[i] === "#") {
+            newArr[newArr.length - 1] = -1 * newArr[newArr.length - 1];
         }
     }
-    console.log(newArr);
-    for (let i = 0; i < newArr.length; i++) {
-        if (newArr[i] === `*`) {
-
-        } else if (newArr[i] === `#`) {
-
-        }
-    }
+    newArr.map((el) => answer += el)
     return answer;
 }
 
-console.log(solution("1S2D*3T"));
-console.log(solution("1S*2T*3S"));
+console.log(solution("1D2S#10S"))
+// console.log(solution("1S2D*3T"));
+// console.log(solution("1S*2T*3S"));
