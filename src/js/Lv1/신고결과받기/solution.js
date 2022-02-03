1e903b76f2f199a3b509d49d9f06621b14a7c40c
Lawen-s <bg920611@gmail.com>
2022-02-03 23:44:37 +0900

    Lv1 신고결과받기

---
+++ src/js/Lv1/신고결과받기/solution.js
@@ -0,0 +1,11 @@
+function solution(id_list, report, k) {
+
+    let obj = Object.assign({}, id_list);
+    console.log(obj);
+    console.log(report[0].split(" "));
+    return 'k';
+}
+
+
+console.log(solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"], 2));
+//console.log(solution(["con", "ryan"],["ryan con", "ryan con", "ryan con", "ryan con"],3));
