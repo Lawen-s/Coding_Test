a1467d8db63812d3cf7741263b91bf59f1993e55
Lawen-s <bg920611@gmail.com>
2022-02-16 23:39:29 +0900

    Lv2 소수찾기 수정2

--- src/js/Lv2/소수찾기/solution.js
+++ src/js/Lv2/소수찾기/solution.js
@@ -1,29 +1,41 @@
+//내가 푼 방법
 function solution(numbers) {
   let answer = 0;
   numbers = numbers.split("")
+  // console.log(numbers.join(""));
   let numArr = [];
   function makeArr(arr, newArr, num) {
     let joinNum = newArr.join("");
+    console.log(joinNum)
+    console.log(typeof (Number(joinNum)) === "number")
     if (joinNum.length !== 0) {
       numArr.push(Number(joinNum));
     }
     if (num === 0) {
+      console.log(newArr);
       return;
     }
     for (let i = 0; i < arr.length; i++) {
       let choice = arr[i];
       let deleteNum = arr.slice();
       deleteNum.splice(i, 1);
+      console.log(deleteNum);
       makeArr(deleteNum, newArr.concat(choice), num - 1)
     }
   }
   makeArr(numbers, [], numbers.length);
   newArr = [...new Set(numArr)];
-  for (let i = 0; i < newArr.length; i++) {
-    if (newArr[i] === 2 || newArr[i] === 3) {
-      answer++;
+  console.log(newArr)
+  function isPrime(num) {
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false
+      }
     }
-    if (newArr[i] % 2 !== 0 && newArr[i] % 3 !== 0 && newArr[i] !== 1) {
+    return num > 1;
+  }
+  for (let i = 0; i < newArr.length; i++) {
+    if (isPrime(newArr[i])) {
       answer++;
     }
   }
@@ -33,4 +45,5 @@ function solution(numbers) {
 
 
 //console.log(solution("17"));
-console.log(solution("011"));
+//console.log(solution("011"));
+console.log(solution("002"));
