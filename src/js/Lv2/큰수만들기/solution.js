456798d18b12cc2243a99646974458127a5e373f
Lawen-s <bg920611@gmail.com>
2022-02-23 13:15:14 +0900

    Lv2 큰수만들기

--- src/js/Lv2/큰수만들기/solution.js
+++ src/js/Lv2/큰수만들기/solution.js
@@ -1,24 +1,39 @@
+// function solution(number, k) {
+//     let answer = [];
+//     let arr = number.split("");
+//     let count = arr.length - k;
+//     console.log(count);
+//     console.log(arr);
+//     for (let i = 0; i < arr.length; i++) {
+//         let num = arr[i];
+//         for (let j = i + 1; j < arr.length; j++) {
+//             num = num + arr[j] + '';
+//             if (num.length === arr.length - k) {
+//                 console.log(num);
+//                 answer.push(num);
+//                 num = num.substring(0, num.length - 1);
+//             }
+//         }
+//     }
+//     let max = Math.max(...answer);
+//     return max;
+// }
+// 다른 풀이
 function solution(number, k) {
     let answer = [];
-    let arr = number.split("");
-    let count = arr.length - k;
-    console.log(count);
-    console.log(arr);
-    for (let i = 0; i < arr.length; i++) {
-        let num = arr[i];
-        for (let j = i + 1; j < arr.length; j++) {
-            num = num + arr[j] + '';
-            if (num.length === arr.length - k) {
-                console.log(num);
-                answer.push(num);
-                num = num.substring(0, num.length - 1);
-            }
+    for (let i = 0; i < number.length; i++) {
+        let num = number[i];
+        while (k > 0 && answer[answer.length - 1] < num) {
+            answer.pop();
+            k--;
         }
+        answer.push(num);
     }
-    let max = Math.max(...answer);
-    return max;
+    //console.log(answer.splice(0,answer.length-k));
+    answer = answer.splice(0, answer.length - k);
+    return answer.join("");
 }
-
 //console.log(solution("1924",2));
-console.log(solution("1231234", 3));
+//console.log(solution("1231234", 3));
 //console.log(solution("4177252841",4));
+console.log(solution("1000", 1))
