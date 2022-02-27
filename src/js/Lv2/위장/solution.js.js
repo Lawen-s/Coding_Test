da77b17b6f00ab91d061af83acbac5be35ecd82d
Lawen-s <bg920611@gmail.com>
2022-02-27 23:50:30 +0900

    Lv2/위장_ing2

--- src/js/Lv2/위장/solution.js.js
+++ src/js/Lv2/위장/solution.js.js
@@ -2,16 +2,20 @@ function solution(clothes) {
     var answer = 0;
     for (let i = 0; i < clothes.length; i++) {
         let name = clothes[i][0];
+        let arr = [];
+        arr.push(name);
         //while문을 통해서 옷을 추가하는 방법을 생각해보기
+        while (1) {
+            let num = i + 1;
+            if (num === clothes.length) {
+                break;
+            }
+            // if(clothes[num].includes()){
+
+            // }
+            num++;
+        }
         answer++;
     }
     return answer;
 }
-
-console.log(solution([["yellowhat", "headgear"],
-["bluesunglasses", "eyewear"],
-["green_turban", "headgear"]]));
-
-console.log(solution([["crowmask", "face"],
-["bluesunglasses", "face"],
-["smoky_makeup", "face"]]));
