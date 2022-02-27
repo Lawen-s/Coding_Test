b963cab5a533b93f909050461ce273a288bf2b9b
Lawen-s <bg920611@gmail.com>
2022-02-27 22:58:52 +0900

    Lv2/위장_ing2

--- src/js/Lv2/위장/solution.js.js
+++ src/js/Lv2/위장/solution.js.js
@@ -2,6 +2,7 @@ function solution(clothes) {
     var answer = 0;
     for (let i = 0; i < clothes.length; i++) {
         let name = clothes[i][0];
+        //while문을 통해서 옷을 추가하는 방법을 생각해보기
         answer++;
     }
     return answer;
