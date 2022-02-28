c43b384e1855ccc7514cf026355766a4ca8ed89a
Lawen-s <bg920611@gmail.com>
2022-02-28 14:15:03 +0900

    Lv2/위장_ing

--- src/js/Lv2/위장/solution.js.js
+++ src/js/Lv2/위장/solution.js.js
@@ -1,21 +1,22 @@
+//다른사람풀이
 function solution(clothes) {
-    var answer = 0;
-    for (let i = 0; i < clothes.length; i++) {
-        let name = clothes[i][0];
-        let arr = [];
-        arr.push(name);
-        //while문을 통해서 옷을 추가하는 방법을 생각해보기
-        while (1) {
-            let num = i + 1;
-            if (num === clothes.length) {
-                break;
-            }
-            // if(clothes[num].includes()){
+    const len = clothes.length;
+    let answer = 1;
+    let obj = {};
 
-            // }
-            num++;
-        }
-        answer++;
+    for (let i = 0; i < len; i++) {
+        obj[clothes[i][1]] = (obj[clothes[i][1]] || 1) + 1;
+        console.log(obj)
     }
-    return answer;
+    for (let key in obj) {
+        answer *= obj[key];
+    }
+
+    return answer - 1;
 }
+console.log(solution([["yellowhat", "headgear"],
+["bluesunglasses", "eyewear"],
+["green_turban", "headgear"]]));
+// console.log(solution([["crowmask", "face"],
+//  ["bluesunglasses", "face"], 
+// ["smoky_makeup", "face"]]));
