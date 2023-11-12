10810f339c21ca23d746e232cd8779cc969d46ae
Lawen-s <bg920611@gmail.com>
2023-11-13 00:39:43 +0900

    Feat: kids-with-the-greatest-number-of-candies

---
+++ src/leetcode/leetcode75/kids-with-the-greatest-number-of-candies/solution.js
@@ -0,0 +1,14 @@
+/**
+ *  https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/?envType=study-plan-v2&envId=leetcode-75
+ * 
+ */
+
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+  const maxNum = Math.max(...candies);
+  return candies.map((candy)=>candy+extraCandies>=maxNum)
+};
