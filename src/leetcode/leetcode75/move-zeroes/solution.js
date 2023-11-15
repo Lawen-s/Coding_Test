c193c4ba3a227d36346c37e86b13c23fc34f16b4
Lawen-s <bg920611@gmail.com>
2023-11-15 21:17:14 +0900

    O:move-zeroes

---
+++ src/leetcode/leetcode75/move-zeroes/solution.js
@@ -0,0 +1,66 @@
+/**
+ * https://leetcode.com/problems/move-zeroes/submissions/1099354251/?envType=study-plan-v2&envId=leetcode-75
+ * 
+ */
+
+
+
+/**
+ * 내가 푼 방법(효율이 낮음)
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  for(let i =0; i < nums.length; i++) {
+    for(let j = i+1; j < nums.length; j++){
+        if(nums[i]===0){
+        let copyNum = nums[j]
+        nums[j]=nums[i]
+        nums[i]=copyNum
+        }
+      }
+  }
+  return nums
+}
+
+/**
+ * 다른 사람이 푼 방법(효율 좋음)
+* @param {number[]} nums
+* @return {void} Do not return anything, modify nums in-place instead.
+*/
+var moveZeroes = function(nums) {
+nums.sort((a,b) => {
+    if (a === 0) {
+        return 1;
+    } else if (b === 0) {
+        return -1;
+    } else {
+        return 0
+    }
+})
+};
+
+
+/**
+ * 효율 제일 좋음
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  let nonZeroIndex = 0
+  
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] !== 0) {
+          nums[nonZeroIndex] = nums[i]
+          nonZeroIndex++
+      }
+  }
+  
+  for (let i = nonZeroIndex; i < nums.length; i++) {
+      nums[i] = 0
+  }
+};
+
+
+console.log(moveZeroes([0,1,0,3,12]))
+console.log(moveZeroes([0]))
