4a6c396d21c05951f0f7d5b3f5556233f502bf3f
Lawen-s <bg920611@gmail.com>
2023-11-21 23:48:11 +0900

    X:find-pivot-index

---
+++ src/leetcode/leetcode75/find-pivot-index/x.js
@@ -0,0 +1,31 @@
+/**
+ * https://leetcode.com/problems/find-pivot-index/submissions/1103506906/?envType=study-plan-v2&envId=leetcode-75
+ */
+
+/**
+ * 블로그를 참고
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+  let total = 0;
+  let leftSum = 0;
+  nums.map((x) => (total += x));
+  for (let i = 0; i < nums.length; i++) {
+    total -= nums[i];
+    if (total === leftSum) {
+      return i;
+    }
+    leftSum += nums[i];
+  }
+  return -1;
+};
+
+/**
+ * 참고 블로그
+ * https://velog.io/@jerry92/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-LeetCode-Find-Pivot-Index
+ */
+
+console.log(pivotIndex([1, 7, 3, 6, 5, 6]));
+console.log(pivotIndex([1, 2, 3]));
+console.log(pivotIndex([2, 1, -1]));
