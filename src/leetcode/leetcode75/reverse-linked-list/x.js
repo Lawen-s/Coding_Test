0655e6fb3c053b31237faf229f20c46a0a8b5bb4
Lawen-s <bg920611@gmail.com>
2023-11-29 23:51:04 +0900

    X:reverse-linked-list

---
+++ src/leetcode/leetcode75/reverse-linked-list/x.js
@@ -0,0 +1,34 @@
+/**
+ * https://leetcode.com/problems/reverse-linked-list/submissions/1108935057/?envType=study-plan-v2&envId=leetcode-75
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * 난이도 쉬움이라고 하지만 어렵다.
+ * 재귀함수 or 반복문으로 해결해야하는데, reduce를 사용해도 통과가 안됐다.
+ * 다른 사람이 푼 문제를 보고 해결
+ * 참고 블로그(https://underdog11.tistory.com/entry/JavaScript-22-Generate-Parentheses-1)
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let current = head;
+  let previous = null;
+  while (current) {
+    const next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
+  }
+  return previous;
+};
+
+console.log(reverseList([1, 2, 3, 4, 5]));
+// console.log(reverseList([1,2]))
+// console.log(reverseList([]))
