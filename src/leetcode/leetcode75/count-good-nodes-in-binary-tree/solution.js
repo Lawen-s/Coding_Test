bc810aca2251b644d19e8943bf3f615c52dfae24
Lawen-s <bg920611@gmail.com>
2023-12-03 22:43:47 +0900

    O: count-good-nodes-in-binary-tree

---
+++ src/leetcode/leetcode75/count-good-nodes-in-binary-tree/solution.js
@@ -0,0 +1,57 @@
+/**
+ * https://leetcode.com/problems/count-good-nodes-in-binary-tree/submissions/1111504574/?envType=study-plan-v2&envId=leetcode-75
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ *
+ * 1 번 방법
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function (root) {
+  // let max = root[0];
+  let count = 0;
+  function def(node, max) {
+    if (!node) return;
+    if (node.val >= max) {
+      count++;
+      max = node.val;
+    }
+    def(node.right, max);
+    def(node.left, max);
+  }
+  def(root, root.val);
+  return count;
+};
+
+/**
+ *
+ * 2번 방법 (1번보다 효율 좋음)
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function (root) {
+  let count = 0;
+  function def(root, max) {
+    if (root.val >= max) {
+      count++;
+      max = root.val;
+    }
+    if (root.right !== null) {
+      def(root.right, max);
+    }
+    if (root.left !== null) {
+      def(root.left, max);
+    }
+  }
+  def(root, root.val);
+  return count;
+};
