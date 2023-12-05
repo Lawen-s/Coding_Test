77986468de40eefb5056e784cd5392a4de58f3c2
Lawen-s <bg920611@gmail.com>
2023-12-05 23:40:25 +0900

    X:binary-tree-right-side-view

---
+++ src/leetcode/leetcode75/binary-tree-right-side-view/x.js
@@ -0,0 +1,54 @@
+/**
+ * https://leetcode.com/problems/binary-tree-right-side-view/submissions/1112945244/?envType=study-plan-v2&envId=leetcode-75
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * DFS로 풀려고 했으나 오류 발생
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function (root) {
+  let rightArr = [];
+  function bfs(root) {
+    if (!root) return null;
+    rightArr.push(root.val);
+    bfs(root.right);
+  }
+  bfs(root);
+  return rightArr;
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ *  풀이 참고(효율 중간)
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function (root) {
+  if (!root) return [];
+  let res = [];
+  pre(root, 0);
+  return res;
+
+  function pre(node, h) {
+    if (!node) return;
+    res[h] = node.val;
+    pre(node.left, h + 1);
+    pre(node.right, h + 1);
+  }
+};
