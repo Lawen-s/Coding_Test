5c17514f5a20514e60d775bc45c339b0258742eb
Lawen-s <bg920611@gmail.com>
2023-12-14 23:47:47 +0900

    X:evaluate-division

---
+++ src/leetcode/leetcode75/evaluate-division/x.js
@@ -0,0 +1,77 @@
+/**
+ * https://leetcode.com/problems/evaluate-division/?envType=study-plan-v2&envId=leetcode-75
+ */
+
+/**
+ * 이해가 잘 안되지만 일단 50% 정도 이해는 감
+ * @param {string[][]} equations
+ * @param {number[]} values
+ * @param {string[][]} queries
+ * @return {number[]}
+ */
+var calcEquation = function(equations, values, queries) {
+  
+  // Step 1: Build the graph
+  let graph = {};
+  
+  for (let i = 0; i < equations.length; i++) {
+    
+    let [numerator, denominator] = equations[i];
+    
+    let value = values[i];
+    
+    if (!graph[numerator]) {
+      graph[numerator] = {};
+    }
+
+    if (!graph[denominator]) {
+      graph[denominator] = {};
+    }
+
+    graph[numerator][denominator] = value;
+    graph[denominator][numerator] = 1 / value;
+  }
+
+  // Step 2: Evaluate queries using DFS
+  let evaluateQuery = (numerator, denominator, visited) => {
+    
+    if (!(numerator in graph) || !(denominator in graph)) {
+      return -1.0;
+    }
+
+    if (numerator === denominator) {
+      return 1.0;
+    }
+
+    visited.add(numerator);
+    let neighbors = graph[numerator];
+
+    for (let neighbor in neighbors) {
+      
+      if (!visited.has(neighbor)) {
+        
+        let result = evaluateQuery(neighbor, denominator, visited);
+        
+        if (result !== -1.0) {
+          return neighbors[neighbor] * result;
+        }
+      }
+    }
+
+    return -1.0;
+  };
+
+  // Step 3: Process queries
+  let results = [];
+  
+  for (let query of queries) {
+    
+    let [numerator, denominator] = query;
+    let visited = new Set();
+    let result = evaluateQuery(numerator, denominator, visited);
+    
+    results.push(result);
+  }
+
+  return results;
+};
