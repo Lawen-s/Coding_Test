c52ddfdb05a7f834d326b9ff9ea4cf42ed7ccfce
Lawen-s <bg920611@gmail.com>
2023-12-18 22:32:21 +0900

    X:guess-number-higher-or-lower

---
+++ src/leetcode/leetcode75/guess-number-higher-or-lower/x.js
@@ -0,0 +1,65 @@
+/**
+ * https://leetcode.com/problems/guess-number-higher-or-lower/submissions/1122605289/?envType=study-plan-v2&envId=leetcode-75
+ */
+
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * 풀이보고 이해, 중간값을 나눠서 크고 작고를 기준으로 조회
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function(n) {
+  let left = 1, right = n
+  while (left < right) {
+      const mid = left + Math.floor((right - left) / 2)
+      const current = guess(mid)
+      if (current === 0) return mid
+      if (current === -1) right = mid
+      else left = mid + 1
+  }
+  return left
+};
+
+
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * 
+ * 효율이 좋은 풀이
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function(n) {
+
+
+  let left = 1
+  let right = n
+
+
+  while(left <= right){
+      let mid = Math.floor((left + right)/2)
+      let pick = guess(mid)
+
+      if(pick === 0) return mid
+
+      if(pick == 1)left = mid+1
+      else right = mid-1
+  }
+
+  return -1
+};
