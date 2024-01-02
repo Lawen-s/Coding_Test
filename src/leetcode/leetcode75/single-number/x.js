0397de4d3dfd86c594322295672514c83d3d7d97
Lawen-s <bg920611@gmail.com>
2024-01-02 23:39:25 +0900

    X: single-number

---
+++ src/leetcode/leetcode75/single-number/x.js
@@ -0,0 +1,39 @@
+/**
+ * 문제 : https://leetcode.com/problems/single-number/solutions/396584/clean-javascript-solution/?envType=study-plan-v2&envId=leetcode-75
+ */
+
+/**
+ * 정답을 보고 이해 
+ * 정렬 후 푸는 문제
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  let arr = nums.sort((a, b) => a - b); // Create a copy of the array and sort it
+
+  for (let i = 0; i <= nums.length - 1; i += 2) { // Iterate by 2
+    if (arr[i] !== arr[i + 1]) {
+      return arr[i];
+    }
+  }
+
+  return arr[arr.length - 1];  // Return the first non-matching pair
+};
+
+
+/**
+ * map을 이용하여 중복을 카운트하는 방식
+ * @param {*} nums 
+ * @returns 
+ */
+function singleNumber(nums) {
+  const map = {};
+  for (let n of nums) {
+    if (map[n] == null) map[n] = 0;
+    map[n]++;
+  }
+
+  for (let n in map) {
+    if (map[n] === 1) return Number(n);
+  }
+}
