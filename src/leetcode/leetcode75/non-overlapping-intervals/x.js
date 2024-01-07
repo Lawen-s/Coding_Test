c9960b926b3fe1efa34f841c91f0a86b0ca1fc15
Lawen-s <bg920611@gmail.com>
2024-01-07 13:16:04 +0900

    X:non-overlapping-intervals

---
+++ src/leetcode/leetcode75/non-overlapping-intervals/x.js
@@ -0,0 +1,47 @@
+/**
+ * https://leetcode.com/problems/non-overlapping-intervals/submissions/1139177987/?envType=study-plan-v2&envId=leetcode-75
+ */
+
+/**
+ * 내가 푼 방식(통과X)
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function(intervals) {
+  intervals.sort((a, b) => a[1] - b[1]);
+  console.log(intervals)
+  let count = 0;
+    for(let i=0; i<intervals.length; i++) {
+      let prev = intervals[i]
+      for(let j =intervals[i].length-1;j<intervals[i].length; j++) {
+        if(intervals[i+1]===undefined){
+          return count
+        }
+        if(prev[j]!==intervals[i+1][0]){
+          count ++;
+        }
+      }
+    }
+};
+
+/**
+ * 풀이를 보고 이해함
+ * @param intervals 
+ * @returns 
+ */
+var eraseOverlapIntervals = function(intervals) {
+	// sort by earliest finish time
+    intervals.sort((a, b) => a[1] - b[1]);
+    let prev = intervals[0], remove = 0;
+    
+    for(let i = 1; i < intervals.length; i++) {
+        if(intervals[i][0] < prev[1]) remove++;
+        else prev = intervals[i];
+    }
+    return remove;
+};
+
+// console.log(eraseOverlapIntervals([[1,2],[2,3],[3,4],[1,3]]))
+// console.log(eraseOverlapIntervals([[1,2],[1,2],[1,2]]))
+// console.log(eraseOverlapIntervals([[1,2],[2,3]]))
+console.log(eraseOverlapIntervals([[1,100],[11,22],[1,11],[2,12]]))
