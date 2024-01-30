01ea69475ba28ef09f45c49ae64cfd212caf3eb4
Lawen-s <bg920611@gmail.com>
2024-01-30 23:36:10 +0900

    X: greatest-common-divisor-of-strings

--- src/leetcode/leetcode75/greatest-common-divisor-of-strings/x.js
+++ src/leetcode/leetcode75/greatest-common-divisor-of-strings/x.js
@@ -21,3 +21,32 @@ var gcdOfStrings = function (str1, str2) {
   const length = GCD(str1.length, str2.length)
   return str2.slice(0, length)
 }
+
+/**
+ * 다시 틀림(최대 공약수를 구하는 방법이라고 생각하기)
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+  if (str1 + str2 !== str2 + str1) {
+ 
+         return "";
+     }
+ 
+     let n = str1.length;
+ 
+     let k = str2.length;
+ 
+     let gcds = function (x, y) {
+         
+         if (!y) {
+             return x;
+         }
+         return gcds(y, x % y);
+     }
+ 
+     let div = gcds (n, k);
+ 
+     return str1.slice(0, div);
+ };
