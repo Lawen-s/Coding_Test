67803762256ea1729a26d5c07fd6f36a72d8401b
Lawen-s <bg920611@gmail.com>
2024-01-31 22:16:32 +0900

    O: kids-with-the-greatest-number-of-candies

--- src/leetcode/leetcode75/kids-with-the-greatest-number-of-candies/solution.js
+++ src/leetcode/leetcode75/kids-with-the-greatest-number-of-candies/solution.js
@@ -12,3 +12,18 @@ var kidsWithCandies = function(candies, extraCandies) {
   const maxNum = Math.max(...candies);
   return candies.map((candy)=>candy+extraCandies>=maxNum)
 };
+
+
+/**
+ *  다시 풀음(24.01.31)
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+  let max = Math.max(...candies)
+  return candies.map((x)=>{
+    if(x+extraCandies>=max)return true
+  else return false
+})
+};
