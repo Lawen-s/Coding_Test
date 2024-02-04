ec3cc698d87b54d3a8cb51cc3db4074300b82ecd
Lawen-s <bg920611@gmail.com>
2024-02-04 17:16:32 +0900

    X: reverse-vowels-of-a-string

--- src/leetcode/leetcode75/reverse-vowels-of-a-string/x.js
+++ src/leetcode/leetcode75/reverse-vowels-of-a-string/x.js
@@ -3,6 +3,30 @@
  * 
  */
 
+/**
+ *  풀이보고 이해(24.02.04)
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+  let vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"]
+  let stack = []
+  for (let i = 0; i < s.length; i++) {
+      if (vowels.indexOf(s[i]) !== -1) {
+          stack.push(s[i])
+      }
+  }
+  let ans = []
+  for (let i = 0; i < s.length; i++) {
+      if (vowels.indexOf(s[i]) !== -1) {
+          ans.push(stack.pop())
+      } else {
+      ans.push(s[i])
+      }
+  }
+  return ans.join('')
+};
+
 
 /**
  * @param {string} s
