ce3e34bfe7581d2a49d172111b7233062e9367b6
Lawen-s <bg920611@gmail.com>
2024-02-21 00:10:53 +0900

    X: container-with-most-water

--- src/leetcode/leetcode75/container-with-most-water/x.js
+++ src/leetcode/leetcode75/container-with-most-water/x.js
@@ -1,7 +1,47 @@
 /**
  * https://leetcode.com/problems/container-with-most-water/submissions/1099456924/?envType=study-plan-v2&envId=leetcode-75
+ * 23-11-26 - X
+ * 24-02-20 - X
  */
 
+/**
+ * 24-02-20 - X (내가 푼 방식-타임에러 발생)
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let area = 0;
+      for(let i =0; i < height.length; i++) {
+        for(let j = i; j < height.length; j++){
+          let small = height[i]>height[j]?height[j]:height[i];
+          let row = small*(j-i);
+          area = area >= row ?area:row
+        }
+      }
+      return area
+};
+/**
+ * 풀이 참고
+ * @param {*} height 
+ * @returns 
+ */
+var maxArea = function(height) {
+let result = 0,
+      left = 0,
+      right = height.length - 1;
+  
+    while (left < right) {
+      let smallestSide = Math.min(height[left], height[right]);
+      let area = (right - left) * smallestSide;
+  
+      if (area > result) result = area;
+  
+      if (height[left] < height[right]) left++;
+      else right--;
+    }
+  
+    return result;
+  }
 /**
  * 내가 푼 방식(timeout 발생)
  * timeout을 없애려면 while 문으로 양끝에서 조회하는 방식으로 가야함
