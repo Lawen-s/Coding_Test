f9aaa1e787f53c1ff76d2b07d21720794ac30047
Lawen-s <bg920611@gmail.com>
2024-03-17 14:49:29 +0900

    X:equal-row-and-column-pairs

--- src/leetcode/leetcode75/equal-row-and-column-pairs/x.js
+++ src/leetcode/leetcode75/equal-row-and-column-pairs/x.js
@@ -1,6 +1,36 @@
 /**
  * https://leetcode.com/problems/equal-row-and-column-pairs/?envType=study-plan-v2&envId=leetcode-75
+ * 24-03-17 - X
+ * 23-11-28 - X
  */
+
+/**
+ * 24-03-17 - X - 풀이를 보고 이해
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var equalPairs = function(grid) {
+
+  const n = grid.length
+  let count = 0
+  const rows = new Map()
+
+  for(let r = 0; r < n; r++){
+      const row = JSON.stringify(grid[r])
+      rows.set(row, 1 + (rows.get(row) || 0))
+  }
+
+  for(let c = 0; c < n; c++){
+    const col = JSON.stringify(grid.map(row => row[c]))
+      count += (rows.get(col) || 0)
+  }
+  
+  return count
+};
+
+// console.log(equalPairs([[3,2,1],[1,7,6],[2,7,7]]))
+// console.log(equalPairs([[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]))
+
 
 /**
  * 참고한 방식(https://www.geeksforgeeks.org/count-all-pairs-of-rows-and-columns-which-are-equal/)
