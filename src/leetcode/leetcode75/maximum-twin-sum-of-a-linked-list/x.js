8da922c9b0c61b7b04108ae372cec2763a07e32c
Lawen-s <bg920611@gmail.com>
2024-03-26 23:45:15 +0900

    X:maximum-twin-sum-of-a-linked-list

--- src/leetcode/leetcode75/maximum-twin-sum-of-a-linked-list/x.js
+++ src/leetcode/leetcode75/maximum-twin-sum-of-a-linked-list/x.js
@@ -1,9 +1,41 @@
 /**
  * https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/submissions/1141531562/
+ * 24-03-26 - X
+ * 24-01-10 - X
  */
 
 /**
- * 풀이보고 이해
+ * 24-03-26 - X - 풀이 보고 이해(효율 좋음)
+ * @param {*} head 
+ * @returns 
+ */
+var pairSum = function(head) {
+  if(head===null || head.next===null){
+      return head
+  }
+  let fast = head
+  let slow = head
+  let prev = null
+  let max = 0
+  while(fast && fast.next){
+      fast = fast.next.next
+      let temp = slow.next
+      slow.next = prev
+      prev = slow
+      slow = temp
+  }
+  while(slow){
+      max = Math.max(prev.val + slow.val, max)
+      slow = slow.next
+      prev = prev.next
+  }
+  return max
+};
+
+
+
+/**
+ * 24-01-10 - X - 풀이보고 이해
  * Definition for singly-linked list.
  * function ListNode(val, next) {
  *     this.val = (val===undefined ? 0 : val)
