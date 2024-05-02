0f172a08f75225ae8b39e196cd7227ddc4f80025
Lawen-s <bg920611@gmail.com>
2024-05-02 21:52:38 +0900

    X: reorder-routes-to-make-all-paths-lead-to-the-city-zero

--- src/leetcode/leetcode75/reorder-routes-to-make-all-paths-lead-to-the-city-zero/x.js
+++ src/leetcode/leetcode75/reorder-routes-to-make-all-paths-lead-to-the-city-zero/x.js
@@ -1,8 +1,70 @@
 /**
  * https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/submissions/1118919446/?envType=study-plan-v2&envId=leetcode-75
+ * 24-05-02 - X
+ * 23-12-14 - X
  */
 
 /**
+ * 24-05-02 - X - 풀이보고 이해
+ * @param {number} n
+ * @param {number[][]} connections
+ * @return {number}
+ */
+var minReorder = function(n, connections) {
+  // from: (<from city>, [<to cities>])
+  // to: (<to city>, [<from cities>])
+  const from = new Map(), to = new Map();
+
+  // Function to insert in values in map
+  const insert = (map, key, value) => {
+      if(map.has(key)){
+          const arr = map.get(key);
+          arr.push(value);
+          map.set(key, arr);
+      } else {
+          map.set(key, [value]);
+      }
+  }
+
+  // Set all values in both maps
+  for(const [a,b] of connections){
+      insert(from, a, b);
+      insert(to, b, a);
+  }
+
+  // Queue: cities to visit
+  const queue = [0], visited = new Set();
+  let count = 0;
+
+  while(queue.length) {
+      const cur = queue.shift(); // First element in queue
+
+      // Check values in first map
+      if(from.has(cur)){
+          for(const x of from.get(cur)){
+              // If visited, do nothing else add to queue
+              if(visited.has(x)) continue;
+              queue.push(x);
+              count++; // Change direction of this path
+          }
+      }
+
+      if(to.has(cur)){
+          // If visited, do nothing else add to queue
+          for(const x of to.get(cur)){
+              if(visited.has(x)) continue;
+              queue.push(x);
+          }
+      }
+
+      visited.add(cur); // Mark city as visited
+  }
+
+  return count
+};
+
+/**
+ * 23-12-14 - X
  * 내가 푼 방식(접근 방식 잘못)
  * 문제 이해가 안된 상태
  * 해당 문제 root node로 돌아갈 수 있는지 문제였음
