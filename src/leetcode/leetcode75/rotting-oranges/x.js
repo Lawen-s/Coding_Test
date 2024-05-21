9abea55c09e855b50c045ae48b3ec47e478ccca5
Lawen-s <bg920611@gmail.com>
2024-05-21 23:19:06 +0900

    X:rotting-oranges

--- src/leetcode/leetcode75/rotting-oranges/x.js
+++ src/leetcode/leetcode75/rotting-oranges/x.js
@@ -1,8 +1,59 @@
 /**
  * https://leetcode.com/problems/rotting-oranges/submissions/1121156121/?envType=study-plan-v2&envId=leetcode-75
+ * 24-05-21 - X
+ * 23-12-17 - X
  */
 
 /**
+ * 24-05-21 - X
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var orangesRotting = function(grid) {
+  const height = grid.length;
+const width = grid[0].length;
+let fresh = 0;
+const queue = [];
+for (let i = 0; i < height; i++) {
+  for (let j = 0; j < width; j++) {
+    if (grid[i][j] === 2) queue.push([i, j]);
+    if (grid[i][j] === 1) fresh++;
+  }
+}
+let minute = 0;
+while (queue.length) {
+  const size = queue.length;
+  for (let i = 0; i < size; i++) {
+    const [x, y] = queue.shift();
+    if (x - 1 >= 0 && grid[x - 1][y] === 1) {
+      grid[x - 1][y] = 2;
+      fresh--;
+      queue.push([x - 1, y]);
+    }
+    if (x + 1 < height && grid[x + 1][y] === 1) {
+      grid[x + 1][y] = 2;
+      fresh--;
+      queue.push([x + 1, y]);
+    }
+    if (y - 1 >= 0 && grid[x][y - 1] === 1) {
+      grid[x][y - 1] = 2;
+      fresh--;
+      queue.push([x, y - 1]);
+    }
+    if (y + 1 < width && grid[x][y + 1] === 1) {
+      grid[x][y + 1] = 2;
+      fresh--;
+      queue.push([x, y + 1]);
+    }
+  }
+  if (queue.length > 0) minute++;
+}
+return fresh === 0 ? minute : -1;
+};
+
+
+/**
+ * 23-12-17 - X
  * 기존에 풀었던 코드를 빌려서 풀어보려했으나 실패
  * @param {number[][]} grid
  * @return {number}
