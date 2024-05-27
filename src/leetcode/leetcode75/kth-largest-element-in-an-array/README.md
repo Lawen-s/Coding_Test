e0d9ddec3a7f7f75548dd761df3e5885e7190d84
Lawen-s <bg920611@gmail.com>
2024-05-27 23:41:37 +0900

    X: kth-largest-element-in-an-array

---
+++ src/leetcode/leetcode75/kth-largest-element-in-an-array/README.md
@@ -0,0 +1,36 @@
+# 215.Kth-Largest-Element-In-An-Array
+
+# 링크
+[leetcode.215](https://leetcode.com/problems/kth-largest-element-in-an-array/?envType=study-plan-v2&envId=leetcode-75)
+
+# 날짜
+* 24-05-21 - X
+* 23-12-17 - X
+
+
+# 문제 설명
+#### 원문
+
+
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+
+Can you solve it without sorting?
+
+
+# EXAMPLES
+#### Example 1.
+
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+
+
+#### Example 2.
+
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
--- src/leetcode/leetcode75/kth-largest-element-in-an-array/x.js
+++ src/leetcode/leetcode75/kth-largest-element-in-an-array/x.js
@@ -1,10 +1,96 @@
 /**
  * https://leetcode.com/problems/kth-largest-element-in-an-array/submissions/1121174254/?envType=study-plan-v2&envId=leetcode-75
+ * 24-05-27 - X
+ * 23-12-17 - X
  */
 
 /**
+ * 24-05-27 - X - 정렬을 구현하는 문제
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+class MinHeap {
+  constructor() {
+      this.heap = [];
+  }
+  push(val) {
+      this.heap.push(val);
+      this.bubbleUp();
+  }
+  pop() {
+      const max = this.heap[0];
+      const end = this.heap.pop();
+      if (this.heap.length > 0) {
+          this.heap[0] = end;
+          this.bubbleDown();
+      }
+      return max;
+  }
+  peek() {
+      return this.heap[0];
+  }
+  bubbleUp() {
+      let idx = this.heap.length - 1;
+      const element = this.heap[idx];
+      while (idx > 0) {
+          let parentIdx = Math.floor((idx - 1) / 2);
+          let parent = this.heap[parentIdx];
+          if (element >= parent) break;
+          this.heap[parentIdx] = element;
+          this.heap[idx] = parent;
+          idx = parentIdx;
+      }
+  }
+  bubbleDown() {
+      let idx = 0;
+      const length = this.heap.length;
+      const element = this.heap[0];
+      while (true) {
+          let leftChildIdx = 2 * idx + 1;
+          let rightChildIdx = 2 * idx + 2;
+          let leftChild, rightChild;
+          let swap = null;
+          if (leftChildIdx < length) {
+              leftChild = this.heap[leftChildIdx];
+              if (leftChild < element) {
+                  swap = leftChildIdx;
+              }
+          }
+          if (rightChildIdx < length) {
+              rightChild = this.heap[rightChildIdx];
+              if (
+                  (swap === null && rightChild < element) || 
+                  (swap !== null && rightChild < leftChild)
+              ) {
+                  swap = rightChildIdx;
+              }
+          }
+          if (swap === null) break;
+          this.heap[idx] = this.heap[swap];
+          this.heap[swap] = element;
+          idx = swap;
+      }
+  }
+}
+var findKthLargest = function(nums, k) {
+  let heap = new MinHeap();
+  for (let i = 0; i < k; i++) {
+      heap.push(nums[i]);
+  }
+  for (let i = k; i < nums.length; i++) {
+      if (nums[i] > heap.peek()) {
+          heap.pop();
+          heap.push(nums[i]);
+      }
+  }
+  return heap.peek();
+};
+
+/**
+ * 23-12-17 - X
  * 문제를 제대로 이해하지 못하였지만, Sort를 사용하지 않고 푸는 방식이라고 생각했음
-* @param {number[]} nums
+ * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
