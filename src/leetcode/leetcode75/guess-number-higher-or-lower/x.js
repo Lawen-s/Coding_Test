dce99c29cbf2ad6aeba0ee3c4eb8628028ea78f0
Lawen-s <bg920611@gmail.com>
2024-07-02 22:20:26 +0900

    X:guess-number-higher-or-lower

--- src/leetcode/leetcode75/guess-number-higher-or-lower/x.js
+++ src/leetcode/leetcode75/guess-number-higher-or-lower/x.js
@@ -2,6 +2,21 @@
  * https://leetcode.com/problems/guess-number-higher-or-lower/submissions/1122605289/?envType=study-plan-v2&envId=leetcode-75
  */
 
+/**
+ * 24-07-02 - X
+ */
+var guessNumber = function(n) {
+  let l = 0, r = n-1;
+  while(l  <= r ){
+      let mid = Math.floor((l+r)/2);
+      let res = guess(mid);
+      if(res == 0) return mid;
+      else if (res == 1) l = mid+1;
+      else r = mid-1;
+  }
+  return l;    
+};
+
 /** 
  * Forward declaration of guess API.
  * @param {number} num   your guess
