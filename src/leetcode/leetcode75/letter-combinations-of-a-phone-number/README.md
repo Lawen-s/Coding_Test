eb5c90766ecb64298a910276f09d86314739f367
Lawen-s <bg920611@gmail.com>
2024-07-09 22:09:46 +0900

    X: letter-combinations-of-a-phone-number

---
+++ src/leetcode/leetcode75/letter-combinations-of-a-phone-number/README.md
@@ -0,0 +1,40 @@
+# 17.Letter-Combinations-Of-A-Phone-Number
+
+# 링크
+[leetcode.17](https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/?envType=study-plan-v2&envId=leetcode-75)
+
+# 날짜
+* 24-07-09 - X
+* 23-12-20 - X
+
+# 문제 설명
+#### 원문
+
+
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+![Alt text](https://assets.leetcode.com/uploads/2022/03/15/1200px-telephone-keypad2svg.png)
+
+
+
+# EXAMPLES
+#### Example 1.
+
+
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+
+#### Example 2.
+
+
+Input: digits = ""
+Output: []
+
+
+#### Example 2.
+
+
+Input: digits = "2"
+Output: ["a","b","c"]
--- src/leetcode/leetcode75/letter-combinations-of-a-phone-number/x.js
+++ src/leetcode/leetcode75/letter-combinations-of-a-phone-number/x.js
@@ -1,6 +1,45 @@
 /**
  * https://leetcode.com/problems/letter-combinations-of-a-phone-number/submissions/1123482159/?envType=study-plan-v2&envId=leetcode-75
+ * 24-07-09 - X
+ * 23-12-20 - X
  */
+
+
+/**
+ * 24-07-09 - X - 이전 풀이랑 비슷
+ */
+const numberToString = {
+    2:["a","b","c"],
+    3:["d","e","f"],
+    4:["g","h","i"],
+    5:["j","k","l"],
+    6:["m","n","o"],
+    7:["p","q","r","s"],
+    8:["t","u","v"],
+    9:["w","x","y","z"]
+}
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    const digitsSplit = digits.split('');
+    const newArr = [];
+    let digitsLength = digits.length
+    if(!digitsLength) return []
+    function dfs (count,newStr){
+        if(count===digitsLength) newArr.push(newStr);
+        else{
+            let letters = numberToString[digits[count]];
+            for(let i=0;i<letters.length;i++){
+                dfs(count+1,newStr+letters[i]);
+            }
+        }
+    }
+    dfs(0,"")
+    return newArr 
+};
 
 /**
  * 풀이를 보고 따라함
