8772df1ac0cb9b466b2e1b50c9c87d0ba2cbd404
Lawen-s <bg920611@gmail.com>
2024-07-20 17:40:03 +0900

    O: koko-eating-bananas

--- src/leetcode/leetcode75/koko-eating-bananas/x.js
+++ src/leetcode/leetcode75/koko-eating-bananas/x.js
@@ -1,11 +1,35 @@
 /**
  * https://leetcode.com/problems/koko-eating-bananas/submissions/1123469317/?envType=study-plan-v2&envId=leetcode-75
+ * 24-07-20 - O 
  * 24-07-16 - X
  * 24-07-14 - O
  * 24-07-11 - O
  * 24-07-07 - O
  * 23-12-19 - X
  */
+
+/** 
+ * 24-07-20 - O 
+ */ 
+var minEatingSpeed = function(piles, h) {
+    let min = 1;
+    let max = Math.max(...piles);
+    let arr = [];
+    while(min<=max){
+        let mid = Math.floor((min+max)/2);
+        let count = 0;
+        for(let i=0;i<piles.length;i++){
+            count+=Math.ceil(piles[i]/mid);
+        }
+        if(count>h){
+            min=mid+1;
+        }else{
+            max=mid-1;
+            arr.push(mid);
+        }
+    }
+    return Math.min(...arr);
+};
 
 /**
  * 24-07-16 - X
