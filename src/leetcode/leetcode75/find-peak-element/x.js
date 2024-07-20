6d69f7196e6629bc5d0b6946d9cf324fdfeab5ea
Lawen-s <bg920611@gmail.com>
2024-07-20 17:27:40 +0900

    X: find-peak-element

--- src/leetcode/leetcode75/find-peak-element/x.js
+++ src/leetcode/leetcode75/find-peak-element/x.js
@@ -1,10 +1,37 @@
 /**
  * https://leetcode.com/problems/find-peak-element/submissions/1123435596/?envType=study-plan-v2&envId=leetcode-75
+ * 24-07-20 - X
  * 24-07-16 - X
  * 24-07-14 - O
  * 24-07-06 - O
  * 23-12-19 - X
  */
+
+/**
+ * 24-07-20 - X
+ * @param {*} nums 
+ * @returns 
+ */
+var findPeakElement = function(nums) {
+    let left = 0;
+    let right = nums.length-1;
+    if(nums.length<=1){
+        return 0;
+    }
+    if(nums.length<=2){
+        return nums[0]>nums[1]?0:1;
+    }
+    while(left<right){
+        let mid = Math.floor((left+right)/2);
+        if(nums[mid]>nums[mid+1]){
+            right=mid;
+        }
+        if(nums[mid]<nums[mid+1]){
+            left=mid+1;
+        }
+    }
+    return left
+};
 
 /**
  * 24-07-16 - X
