ca54f13819a7f43ae2ecb92725ea3a69d79c6956
Lawen-s <bg920611@gmail.com>
2024-07-25 23:27:25 +0900

    X: edit-distance

--- src/leetcode/leetcode75/edit-distance/x.js
+++ src/leetcode/leetcode75/edit-distance/x.js
@@ -1,8 +1,38 @@
 /**
  * 블로그: https://madplay.github.io/post/levenshtein-distance-edit-distance
  * 문제 : https://leetcode.com/problems/edit-distance/submissions/1134643640/?envType=study-plan-v2&envId=leetcode-75
+ * 24-07-25 - X
+ * 24-01-02 - X
  */
 
+/**
+ * 24-07-25 - X
+ */
+var minDistance = function(word1, word2) {
+    const m = word1.length;
+    const n = word2.length;
+    const dp = new Array(m+1).fill(null).map(() => new Array(n+1).fill(null));
+    
+    for (let i = 0; i <= m; i++) {
+        dp[i][0] = i;
+    }
+    
+    for (let j = 0; j <= n; j++) {
+        dp[0][j] = j;
+    }
+    
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (word1[i-1] === word2[j-1]) {
+                dp[i][j] = dp[i-1][j-1];
+            } else {
+                dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1;
+            }
+        }
+    }
+    
+    return dp[m][n];
+};
 
 /**
  * 풀이 보고 이해
