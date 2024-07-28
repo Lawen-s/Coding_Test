446dc63235689fb779b36dca1298111882b8a18d
beomgi.sa <beomgi.sa@gintlab.com>
2024-07-28 15:26:57 +0900

    O: longest-common-subsequence

--- src/leetcode/leetcode75/longest-common-subsequence/README.md
+++ src/leetcode/leetcode75/longest-common-subsequence/README.md
@@ -1,17 +1,20 @@
 # 1143.Longest-Common-Subsequence
 
 # 링크
+
 [leetcode.1143](https://leetcode.com/problems/longest-common-subsequence/description/?envType=study-plan-v2&envId=leetcode-75)
 
 # 날짜
-* 24-07-25 - X
-* 24-07-24 - X
-* 24-07-23 - X
-* 24-01-01 - X
+
+- 24-07-28 - O
+- 24-07-25 - X
+- 24-07-24 - X
+- 24-07-23 - X
+- 24-01-01 - X
 
 # 문제 설명
-#### 원문
 
+#### 원문
 
 Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
 
@@ -20,27 +23,21 @@ A subsequence of a string is a new string generated from the original string wit
 For example, "ace" is a subsequence of "abcde".
 A common subsequence of two strings is a subsequence that is common to both strings.
 
-
-
 # EXAMPLES
+
 #### Example 1.
 
-
-Input: text1 = "abcde", text2 = "ace" 
+Input: text1 = "abcde", text2 = "ace"
 Output: 3  
 Explanation: The longest common subsequence is "ace" and its length is 3.
 
-
 #### Example 2.
-
 
 Input: text1 = "abc", text2 = "abc"
 Output: 3
 Explanation: The longest common subsequence is "abc" and its length is 3.
 
-
 #### Example 3.
-
 
 Input: text1 = "abc", text2 = "def"
 Output: 0
--- src/leetcode/leetcode75/longest-common-subsequence/x.js
+++ src/leetcode/leetcode75/longest-common-subsequence/x.js
@@ -1,6 +1,7 @@
 /**
  * 참고 블로그 : https://velog.io/@emplam27/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B7%B8%EB%A6%BC%EC%9C%BC%EB%A1%9C-%EC%95%8C%EC%95%84%EB%B3%B4%EB%8A%94-LCS-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-Longest-Common-Substring%EC%99%80-Longest-Common-Subsequence
  * 문제 : https://leetcode.com/problems/longest-common-subsequence/submissions/1133734801/?envType=study-plan-v2&envId=leetcode-75
+ * 24-07-28 - O
  * 24-07-25 - X
  * 24-07-24 - X
  * 24-07-23 - X
@@ -8,57 +9,80 @@
  */
 
 /**
- * 24-07-25 - X 
+ * 24-07-28 - O
  */
-var longestCommonSubsequence = function(text1, text2) {
-    let dp = Array(text1.length+1).fill(0);
-    dp = dp.map(()=>Array(text2.length+1).fill(0));
-    for(let i =1;i<=text1.length;i++){
-        for(let j=1;j<=text2.length;j++){
-            if(text1[i-1]===text2[j-1]){
-                dp[i][j]=dp[i-1][j-1]+1;
-            }else{
-                dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
-            }
-        }
+var longestCommonSubsequence = function (text1, text2) {
+  let dp = Array(text1.length + 1)
+    .fill()
+    .map(() => Array(text2.length + 1).fill(0));
+  for (let i = 1; i <= text1.length; i++) {
+    for (let j = 1; j <= text2.length; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
     }
-    return dp[text1.length][text2.length]
+  }
+  return dp[text1.length][text2.length];
+};
+
+/**
+ * 24-07-25 - X
+ */
+var longestCommonSubsequence = function (text1, text2) {
+  let dp = Array(text1.length + 1).fill(0);
+  dp = dp.map(() => Array(text2.length + 1).fill(0));
+  for (let i = 1; i <= text1.length; i++) {
+    for (let j = 1; j <= text2.length; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+      }
+    }
+  }
+  return dp[text1.length][text2.length];
 };
 
 /**
  * 24-07-24 - X
  */
-var longestCommonSubsequence = function(text1, text2) {
-    let dp = Array(text1.length+1).fill(0).map(()=>Array(text2.length+1).fill(0));
-    for(let i =1;i<=text1.length;i++){
-        for(let j=1;j<=text2.length;j++){
-            if(text1[i-1]===text2[j-1]){
-                dp[i][j]=dp[i-1][j-1]+1;
-            }else{
-                dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1])
-            }
-        }
+var longestCommonSubsequence = function (text1, text2) {
+  let dp = Array(text1.length + 1)
+    .fill(0)
+    .map(() => Array(text2.length + 1).fill(0));
+  for (let i = 1; i <= text1.length; i++) {
+    for (let j = 1; j <= text2.length; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
     }
-    return dp[text1.length][text2.length];
+  }
+  return dp[text1.length][text2.length];
 };
 
 /**
  * 24-07-23 - X
  */
-var longestCommonSubsequence = function(text1, text2) {
-    const length1 = text1.length;
-    const length2 = text2.length;
-    let dp = Array(length1+1).fill(0).map(()=> Array(length2+1).fill(0))
-    for(let i =1;i<=length1;i++){
-        for(let j=1;j<=length2;j++){
-            if(text1.charAt(i-1)===text2.charAt(j-1)){
-                dp[i][j]=dp[i-1][j-1]+1
-            }else{
-                dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
+var longestCommonSubsequence = function (text1, text2) {
+  const length1 = text1.length;
+  const length2 = text2.length;
+  let dp = Array(length1 + 1)
+    .fill(0)
+    .map(() => Array(length2 + 1).fill(0));
+  for (let i = 1; i <= length1; i++) {
+    for (let j = 1; j <= length2; j++) {
+      if (text1.charAt(i - 1) === text2.charAt(j - 1)) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
     }
-    return dp[length1][length2];
+  }
+  return dp[length1][length2];
 };
 
 /**
@@ -68,22 +92,20 @@ var longestCommonSubsequence = function(text1, text2) {
  * @param {string} text2
  * @return {number}
  */
-var longestCommonSubsequence = function(text1, text2) {
-  let newStr ='';
-  for(let i =0;i<text1.length;i++){
-      for(let j=0; j<text2.length;j++){
-          if(text1[i]===text2[j]){
-            console.log(text1[i])
-              newStr+=text1[i]
-              break;
-          }
+var longestCommonSubsequence = function (text1, text2) {
+  let newStr = "";
+  for (let i = 0; i < text1.length; i++) {
+    for (let j = 0; j < text2.length; j++) {
+      if (text1[i] === text2[j]) {
+        console.log(text1[i]);
+        newStr += text1[i];
+        break;
       }
+    }
   }
 
-  return newStr.length
+  return newStr.length;
 };
-
-
 
 /**
  *  참고한 코드
@@ -91,22 +113,23 @@ var longestCommonSubsequence = function(text1, text2) {
  * @param {string} text2
  * @return {number}
  */
-var longestCommonSubsequence = function(text1, text2) {
-  let dp = new Array(text1.length+1).fill(null).map(() => new Array(text2.length+1).fill(0));
+var longestCommonSubsequence = function (text1, text2) {
+  let dp = new Array(text1.length + 1)
+    .fill(null)
+    .map(() => new Array(text2.length + 1).fill(0));
 
   for (let i = 1; i <= text1.length; i++) {
-      for (let j = 1; j <= text2.length; j++) {
-          if (text1[i-1] === text2[j-1]) {
-              dp[i][j] = dp[i-1][j-1] + 1;
-          } else {
-              dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-          }
+    for (let j = 1; j <= text2.length; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
       }
+    }
   }
-  
+
   return dp[text1.length][text2.length];
 };
-
 
 /**
  * 효율이 좋은 코드
@@ -114,26 +137,25 @@ var longestCommonSubsequence = function(text1, text2) {
  * @param {string} text2
  * @return {number}
  */
-var longestCommonSubsequence = function(text1, text2) {
-  let dp = new Array(text1.length + 1)
+var longestCommonSubsequence = function (text1, text2) {
+  let dp = new Array(text1.length + 1);
 
-  for(let i = 0; i <= text1.length; i++) {
-      dp[i] = new Array(text2.length + 1).fill(0)
+  for (let i = 0; i <= text1.length; i++) {
+    dp[i] = new Array(text2.length + 1).fill(0);
   }
 
-  for(let i = 1; i <= text1.length; i++) {
-      for(let j = 1; j <= text2.length; j++) {
-          if(text1[i-1] === text2[j-1]) {
-              dp[i][j] = dp[i-1][j-1] + 1
-          } else {
-              dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
-          }
+  for (let i = 1; i <= text1.length; i++) {
+    for (let j = 1; j <= text2.length; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
       }
+    }
   }
 
-  return dp[text1.length][text2.length]
+  return dp[text1.length][text2.length];
 };
 
-
 // console.log(longestCommonSubsequence(text1 = "abcde", text2 = "ace" ))
-console.log(longestCommonSubsequence(text1 = "ezupkr", text2 = "ubmrapg" ))
+console.log(longestCommonSubsequence((text1 = "ezupkr"), (text2 = "ubmrapg")));
