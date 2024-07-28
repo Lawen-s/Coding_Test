e1334dd30ec0039eb7b3f44f005db34dd67c583d
beomgi.sa <beomgi.sa@gintlab.com>
2024-07-28 16:15:16 +0900

    X: counting-bits

---
+++ src/leetcode/leetcode75/counting-bits/README.md
@@ -0,0 +1,39 @@
+# 338.Counting-Bits
+
+# 링크
+
+[leetcode.338](https://leetcode.com/problems/counting-bits/description/?envType=study-plan-v2&envId=leetcode-75)
+
+# 날짜
+
+- 24-07-28 - X
+- 24-01-02 - O
+
+# 문제 설명
+
+#### 원문
+
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+# EXAMPLES
+
+#### Example 1.
+
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+
+#### Example 2.
+
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
--- src/leetcode/leetcode75/counting-bits/solution.js
+++ src/leetcode/leetcode75/counting-bits/solution.js
@@ -1,50 +1,67 @@
 /**
  * 문제 : https://leetcode.com/problems/counting-bits/submissions/1134666561/?envType=study-plan-v2&envId=leetcode-75
+ * 24-07-28 - X
+ * 24-01-02 - O
  */
+
+/**
+ * 24-07-28 - X
+ * n의 숫자를 비트로 계산하여 1이 몇개인지 배열에 담기
+ */
+var countBits = function (n) {
+  let arr = [0];
+  for (let i = 1; i <= n; i++) {
+    console.log();
+    let bite = i.toString(2).split("");
+    let count = 0;
+    bite.forEach((el) => {
+      if (el === "1") count++;
+    });
+    arr.push(count);
+  }
+  return arr;
+};
 
 /**
  *  내가 푼 방식
  * @param {number} n
  * @return {number[]}
  */
-var countBits = function(n) {
-  let arr = []
-    for(let i =0;i<=n;i++){
-      const reg = new RegExp('1', 'g');
-      if (i.toString(2).match(reg) === null){
-        arr[i] = 0;
-      }
-      else arr[i] = Number(i.toString(2).match(reg).length)
-    }
-    return arr
+var countBits = function (n) {
+  let arr = [];
+  for (let i = 0; i <= n; i++) {
+    const reg = new RegExp("1", "g");
+    if (i.toString(2).match(reg) === null) {
+      arr[i] = 0;
+    } else arr[i] = Number(i.toString(2).match(reg).length);
+  }
+  return arr;
 };
 
 // console.log(countBits(2))
-console.log(countBits(5))
-
-
+console.log(countBits(5));
 
 /**
  * 직접 2진수를 구하는 방식
  * @param {number} n
  * @return {number[]}
  */
-var countBits = function(n) {
-  let rev=[];
-  for(i=0;i<=n;i++){
-      let x=helper(i)
-  rev.push(x)
+var countBits = function (n) {
+  let rev = [];
+  for (i = 0; i <= n; i++) {
+    let x = helper(i);
+    rev.push(x);
   }
-  return rev
+  return rev;
 };
-function helper(n){
-  let count=0
-   while (n>0) {
-      let r=n%2;
-      if(r==1){
-          count++
-      }
-      n=Math.floor(n/2);;
-}
-return count
+function helper(n) {
+  let count = 0;
+  while (n > 0) {
+    let r = n % 2;
+    if (r == 1) {
+      count++;
+    }
+    n = Math.floor(n / 2);
+  }
+  return count;
 }
