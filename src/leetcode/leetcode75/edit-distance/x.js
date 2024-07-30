1d4c0cb14e8d63e82fef7f44f05202e4aca3bf18
Lawen-s <bg920611@gmail.com>
2024-07-30 22:42:28 +0900

    X: edit-distance

--- src/leetcode/leetcode75/edit-distance/x.js
+++ src/leetcode/leetcode75/edit-distance/x.js
@@ -1,10 +1,35 @@
 /**
  * 블로그: https://madplay.github.io/post/levenshtein-distance-edit-distance
  * 문제 : https://leetcode.com/problems/edit-distance/submissions/1134643640/?envType=study-plan-v2&envId=leetcode-75
+ * 24-07-30 - X
  * 24-07-28 - X
  * 24-07-25 - X
  * 24-01-02 - X
  */
+
+/**
+ * 24-07-30 - X
+ */
+var minDistance = function(word1, word2) {
+  let dp = Array(word1.length+1).fill().map(()=>Array(word2.length+1).fill(0));
+  for(let i=0;i<word1.length+1;i++){
+      dp[i][0]=i;
+  }
+  for(let i=0;i<word2.length+1;i++){
+      dp[0][i]=i;
+  }
+  for(let i=1;i<=word1.length;i++){
+      for(let j=1;j<=word2.length;j++){
+          if(word1[i-1]===word2[j-1]){
+              // dp[i][j]=Math.min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1]);
+              dp[i][j]=dp[i-1][j-1];
+          }else{
+              dp[i][j]=Math.min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])+1;
+          }
+      }
+  }
+  return dp[word1.length][word2.length]
+};
 
 /**
  * 24-07-28 - X
