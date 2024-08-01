67a95b649eb3dd39b054ffe9def4817aaef0a4ff
Lawen-s <bg920611@gmail.com>
2024-08-01 23:10:25 +0900

    O: longest-common-subsequence

--- src/leetcode/leetcode75/longest-common-subsequence/x.js
+++ src/leetcode/leetcode75/longest-common-subsequence/x.js
@@ -1,6 +1,8 @@
 /**
  * 참고 블로그 : https://velog.io/@emplam27/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B7%B8%EB%A6%BC%EC%9C%BC%EB%A1%9C-%EC%95%8C%EC%95%84%EB%B3%B4%EB%8A%94-LCS-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-Longest-Common-Substring%EC%99%80-Longest-Common-Subsequence
  * 문제 : https://leetcode.com/problems/longest-common-subsequence/submissions/1133734801/?envType=study-plan-v2&envId=leetcode-75
+ * 24-08-01 - O
+ * 24-07-31 - O
  * 24-07-30 - X
  * 24-07-28 - O
  * 24-07-25 - X
@@ -8,6 +10,23 @@
  * 24-07-23 - X
  * 24-01-01 - X
  */
+
+/**
+ * 24-08-01 - O
+ */
+var longestCommonSubsequence = function(text1, text2) {
+  let dp = Array.from({length:text1.length+1},()=>Array(text2.length+1).fill(0));
+  for(let i=1;i<=text1.length;i++){
+      for(let j=1;j<=text2.length;j++){
+          if(text1[i-1]===text2[j-1]){
+              dp[i][j] = dp[i-1][j-1] +1;
+          }else{
+              dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+          }
+      }
+  }
+  return dp[text1.length][text2.length];
+};
 
 /**
  * 24-07-30 - X 
