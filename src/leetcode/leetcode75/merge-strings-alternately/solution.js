27b7d6abf1b463e2f9688b02c5d1daa52bc32037
Lawen-s <bg920611@gmail.com>
2024-08-13 22:56:47 +0900

    O: merge-strings-alternately

--- src/leetcode/leetcode75/merge-strings-alternately/solution.js
+++ src/leetcode/leetcode75/merge-strings-alternately/solution.js
@@ -1,8 +1,22 @@
 /**
- * leetcode 주소
  * https://leetcode.com/problems/merge-strings-alternately/description/?envType=study-plan-v2&envId=leetcode-75
- * 
- *  */ 
+ * 24-08-13 - O
+ * 23-11-12 - O
+ */
+
+/**
+ * 24-08-13 - O
+ */
+var mergeAlternately = function(word1, word2) {
+    let max = Math.max(word1.length,word2.length);
+    let newstr = '';
+    for(let i =0;i<max;i++){
+        let word1Str = word1[i]?word1[i]:'';
+        let word2Str = word2[i]?word2[i]:'';
+        newstr+=word1Str+word2Str;
+    }
+    return newstr;
+};
 
 /**
  * @param {string} word1
