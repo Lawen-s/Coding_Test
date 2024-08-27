68ef7ded55620d850962a9c6c2625979233e6882
Lawen-s <bg920611@gmail.com>
2024-08-27 23:33:14 +0900

    X: can-place-flowers

--- src/leetcode/leetcode75/can-place-flowers/x.js
+++ src/leetcode/leetcode75/can-place-flowers/x.js
@@ -1,6 +1,33 @@
 /**
  * 문제: https://leetcode.com/problems/can-place-flowers/?envType=study-plan-v2&envId=leetcode-75
+ * 24-08-27 - X
+ * 24-01-31 - X
  */
+
+/**
+ * 24-08-27 - X 
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+  let count = 0;
+  let pre = -1;
+  let next = 1;
+
+  for (let i = 0; i < flowerbed.length; i++, pre++, next++) {
+      if ((getValue(flowerbed, pre) + getValue(flowerbed, i) + getValue(flowerbed, next)) == 0) {
+          flowerbed[i] = 1;
+          count++;
+      }
+  }
+
+  return count >= n;
+};
+
+var getValue = function(flowerbed, i) {
+  if (i < 0 || i >= flowerbed.length) {
+      return 0;
+  }
+  return flowerbed[i];
+}
 
 /**
  * 풀이 확인 (24.01.31)
