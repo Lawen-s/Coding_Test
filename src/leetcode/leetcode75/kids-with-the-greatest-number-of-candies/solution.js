119e26ba7378edb238ab390260ca21e064c29fdd
Lawen-s <bg920611@gmail.com>
2024-09-07 21:28:07 +0900

    O: kids-with-the-greatest-number-of-candies

--- src/leetcode/leetcode75/kids-with-the-greatest-number-of-candies/solution.js
+++ src/leetcode/leetcode75/kids-with-the-greatest-number-of-candies/solution.js
@@ -1,8 +1,22 @@
 /**
  *  https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/?envType=study-plan-v2&envId=leetcode-75
+ * 24-09-07 - O
  * 24-08-27 - O
  * 23-11-13 - O
  */
+
+/**
+ * 24-09-07 - O
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+  let result = []
+  for(let i =0;i<candies.length;i++){
+      let max = candies[i]+extraCandies;
+      let check = candies.filter((v)=>max<v).length>0?false:true;
+      result.push(check);
+  }
+  return result;
+};
 
 /**
  * 24-08-27 - O 
