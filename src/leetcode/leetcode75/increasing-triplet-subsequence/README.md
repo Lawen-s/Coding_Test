328459a159833f44f3a164d247ec16f59948f301
Lawen-s <bg920611@gmail.com>
2024-09-10 22:14:51 +0900

    X: increasing-triplet-subsequence

---
+++ src/leetcode/leetcode75/increasing-triplet-subsequence/README.md
@@ -0,0 +1,36 @@
+# 334.Increasing-Triplet-Subsequence
+
+# 링크
+
+[leetcode.334](https://leetcode.com/problems/increasing-triplet-subsequence/?envType=study-plan-v2&envId=leetcode-75)
+
+# 날짜 (JS, PY)
+
+- 24-09-10 - X
+- 24-02-14 - X
+
+# 문제 설명
+
+#### 원문
+
+Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+# EXAMPLES
+
+#### Example 1.
+
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+
+#### Example 2.
+
+Input: nums = [5,4,3,2,1]
+Output: false
+Explanation: No triplet exists.
+
+#### Example 3.
+
+Input: nums = [2,1,5,0,4,6]
+Output: true
+Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
--- src/leetcode/leetcode75/increasing-triplet-subsequence/x.js
+++ src/leetcode/leetcode75/increasing-triplet-subsequence/x.js
@@ -1,40 +1,16 @@
 /**
  * https://leetcode.com/problems/increasing-triplet-subsequence/?envType=study-plan-v2&envId=leetcode-75
- * 24.02.14 - X
+ * 24-09-10 - X
+ * 24-02-14 - X
  */
 
 /**
- * 2024-02-14
- * 통과 X (66/83) - 최대값으로 값들을 비교하는 방식
- * @param {number[]} nums
- * @return {boolean}
+ * 24-09-10 - X
  */
-var increasingTriplet = function(nums) {
-  let before=nums[0], middle=nums[1], next=nums[2];
-  for(let i =2;i<nums.length;i++){
-      if(before<middle&&middle<next){
-          return true
-      }
-      else{
-          before = middle;
-          middle=next;
-          next=nums[i+1]
-      }
-  }
-  return false
-};
-
-/**
- * 2024-02-14
- *  풀이 참고(통과코드)
- * @param {number[]} nums
- * @return {boolean}
- */
-var increasingTriplet = function(nums) {
+var increasingTriplet = function (nums) {
   let firstNumber = Infinity;
   let secondNumber = Infinity;
-  console.log(firstNumber)
-  
+
   for (let currentNumber of nums) {
     if (currentNumber > secondNumber && currentNumber > firstNumber) {
       return true;
@@ -46,55 +22,100 @@ var increasingTriplet = function(nums) {
     }
   }
   return false;
-}
+};
 
+/**
+ * 2024-02-14
+ * 통과 X (66/83) - 최대값으로 값들을 비교하는 방식
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function (nums) {
+  let before = nums[0],
+    middle = nums[1],
+    next = nums[2];
+  for (let i = 2; i < nums.length; i++) {
+    if (before < middle && middle < next) {
+      return true;
+    } else {
+      before = middle;
+      middle = next;
+      next = nums[i + 1];
+    }
+  }
+  return false;
+};
+
+/**
+ * 2024-02-14
+ *  풀이 참고(통과코드)
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function (nums) {
+  let firstNumber = Infinity;
+  let secondNumber = Infinity;
+  console.log(firstNumber);
+
+  for (let currentNumber of nums) {
+    if (currentNumber > secondNumber && currentNumber > firstNumber) {
+      return true;
+    }
+    if (currentNumber > firstNumber) {
+      secondNumber = currentNumber;
+    } else {
+      firstNumber = currentNumber;
+    }
+  }
+  return false;
+};
 
 // 내 코드
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
-var increasingTriplet = function(nums) {
+var increasingTriplet = function (nums) {
   let check = 2;
-  let numsHalf = nums.length /2;
+  let numsHalf = nums.length / 2;
   let smallNum = nums[0];
   let mideumNum = nums[0];
-  for(let i = 0; i < nums.length; i++) {
-    if (nums[i]<smallNum&&numsHalf>=i) {
-      smallNum=nums[i]
-      mideumNum=nums[i]
-      check=2;
-  }else{
-    if(smallNum<nums[i]){
-      if(mideumNum<nums[i]){
-        mideumNum=nums[i]
-        check-=1;
-      }else{
-        mideumNum=nums[i]
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < smallNum && numsHalf >= i) {
+      smallNum = nums[i];
+      mideumNum = nums[i];
+      check = 2;
+    } else {
+      if (smallNum < nums[i]) {
+        if (mideumNum < nums[i]) {
+          mideumNum = nums[i];
+          check -= 1;
+        } else {
+          mideumNum = nums[i];
+        }
       }
     }
+    if (check === 0) {
+      return true;
+    }
   }
-  if(check===0){
-    return true
-  }
+  return false;
 };
-return false
-}
 
 /**
  * 통과 코드 1
  * @param {number[]} nums
  * @return {boolean}
  */
-var increasingTriplet = function(nums) {
-  let min = Math.max(...nums)
-  let mid = min
-  for (let num of nums){
-      if(num <= min) min = num
-      else if(num <= mid) mid = num
-      else return true 
+var increasingTriplet = function (nums) {
+  let min = Math.max(...nums);
+  let mid = min;
+  for (let num of nums) {
+    if (num <= min) min = num;
+    else if (num <= mid) mid = num;
+    else return true;
   }
-  return false
+  return false;
 };
 
 /**
@@ -103,19 +124,18 @@ var increasingTriplet = function(nums) {
  * @param {number[]} nums
  * @return {boolean}
  */
-var increasingTriplet = function(nums) {
+var increasingTriplet = function (nums) {
   let min = nums[0];
   let middleNumUpdatedAfterMin = Infinity;
-  
+
   for (const num of nums) {
-      if (num <= min) min = num;
-      else if (num <= middleNumUpdatedAfterMin) middleNumUpdatedAfterMin = num;
-      else return true; // We found out third number, the largest
+    if (num <= min) min = num;
+    else if (num <= middleNumUpdatedAfterMin) middleNumUpdatedAfterMin = num;
+    else return true; // We found out third number, the largest
   }
-  
+
   return false;
 };
-
 
 // console.log(increasingTriplet([9,10,5,11,10,9,8]))
 // console.log(increasingTriplet([1,5,0,4,1,3]))
