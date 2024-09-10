c144dc6b4a4b43b2a46a835097816afaeffcad74
Lawen-s <bg920611@gmail.com>
2024-09-10 21:47:03 +0900

    O: reverse-words-in-a-string

--- src/leetcode/leetcode75/reverse-words-in-a-string/README.md
+++ src/leetcode/leetcode75/reverse-words-in-a-string/README.md
@@ -1,15 +1,17 @@
 # 151.Reverse-Words-In-A-String
 
 # 링크
+
 [leetcode.151](https://leetcode.com/problems/reverse-words-in-a-string/?envType=study-plan-v2&envId=leetcode-75)
 
 # 날짜(JS, PY)
 
-* 24-08-29 - O
-* 24-02-04 - O
-
+- 24-09-10 - O
+- 24-08-29 - O
+- 24-02-04 - O
 
 # 문제 설명
+
 #### 원문
 
 Given an input string s, reverse the order of the words.
@@ -20,23 +22,21 @@ Return a string of the words in reverse order concatenated by a single space.
 
 Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
 
-
 # EXAMPLES
+
 #### Example 1.
 
 Input: s = "the sky is blue"
 Output: "blue is sky the"
 
-
 #### Example 2.
 
-Input: s = "  hello world  "
+Input: s = " hello world "
 Output: "world hello"
 Explanation: Your reversed string should not contain leading or trailing spaces.
 
-
 #### Example 3.
 
-Input: s = "a good   example"
+Input: s = "a good example"
 Output: "example good a"
 Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
--- src/leetcode/leetcode75/reverse-words-in-a-string/solution.js
+++ src/leetcode/leetcode75/reverse-words-in-a-string/solution.js
@@ -1,24 +1,44 @@
 /**
  * https://leetcode.com/problems/reverse-words-in-a-string/?envType=study-plan-v2&envId=leetcode-75
+ * 24-09-10 - O
  * 24-08-29 - O
  * 24-02-04 - O
  */
 
 /**
+ * 24-09-10 - O
+ */
+var reverseVowels = function (s) {
+  let wordArr = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  let vowelArr = s
+    .split("")
+    .filter((v) => wordArr.includes(v))
+    .reverse();
+  let sArr = s.split("");
+  for (let i = 0; i < sArr.length; i++) {
+    if (wordArr.includes(sArr[i])) {
+      let vowel = vowelArr.shift();
+      sArr[i] = vowel;
+    }
+  }
+  return sArr.join("");
+};
+
+/**
  * 24-08-29 - O
  */
-var reverseWords = function(s) {
-  let wordsArr = s.split(' ').filter(word=>word!=='');
-  let start =0;
-  let end = wordsArr.length -1;
-  while(start<end){
-      let temp = wordsArr[start]
-      wordsArr[start]=wordsArr[end];
-      wordsArr[end]=temp;
-      start++;
-      end--;
+var reverseWords = function (s) {
+  let wordsArr = s.split(" ").filter((word) => word !== "");
+  let start = 0;
+  let end = wordsArr.length - 1;
+  while (start < end) {
+    let temp = wordsArr[start];
+    wordsArr[start] = wordsArr[end];
+    wordsArr[end] = temp;
+    start++;
+    end--;
   }
-  return wordsArr.join(' ');
+  return wordsArr.join(" ");
 };
 
 /**
@@ -26,31 +46,30 @@ var reverseWords = function(s) {
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function(s) {
-  let splitStr = s.split(' ');
-  console.log(splitStr)
-  let newArr = splitStr.filter((x)=>x !== '').reverse()
-  return newArr.join(' ')
+var reverseWords = function (s) {
+  let splitStr = s.split(" ");
+  console.log(splitStr);
+  let newArr = splitStr.filter((x) => x !== "").reverse();
+  return newArr.join(" ");
 };
-
 
 /**
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function(s) {
+var reverseWords = function (s) {
   let splitString = s.split(" ");
   let reverseArray = splitString.reverse();
   let newArray = [];
-  console.log(reverseArray)
-  for(let i=0;i<reverseArray.length;i++){
-      if(reverseArray[i]!==""){
-          newArray.push(reverseArray[i])
-      }
+  console.log(reverseArray);
+  for (let i = 0; i < reverseArray.length; i++) {
+    if (reverseArray[i] !== "") {
+      newArray.push(reverseArray[i]);
+    }
   }
-  return newArray.join(" ")
+  return newArray.join(" ");
 };
 
-reverseWords("heel thoe keing")
-console.log(reverseWords("the sky is blue"))
-console.log(reverseWords("  hello world  "))
+reverseWords("heel thoe keing");
+console.log(reverseWords("the sky is blue"));
+console.log(reverseWords("  hello world  "));
