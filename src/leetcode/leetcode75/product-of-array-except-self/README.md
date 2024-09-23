828dd5ac23ce1c8364ecdc3317294276e2ce67d5
Lawen-s <bg920611@gmail.com>
2024-09-23 22:21:17 +0900

    X:product-of-array-except-self

--- src/leetcode/leetcode75/product-of-array-except-self/README.md
+++ src/leetcode/leetcode75/product-of-array-except-self/README.md
@@ -1,17 +1,19 @@
 # 238.Product-Of-Array-Except-Self
 
 # 링크
+
 [leetcode.238](https://leetcode.com/problems/product-of-array-except-self/submissions/1379899741/?envType=study-plan-v2&envId=leetcode-75)
 
 # 날짜(JS, PY)
 
-* 24-09-05 - O
-* 24-02-05 - X
-* 23-11-14 - O
+- 24-09-23 - X
+- 24-09-05 - O
+- 24-02-05 - X
+- 23-11-14 - O
 
 # 문제 설명
-#### 원문
 
+#### 원문
 
 Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 
@@ -19,20 +21,16 @@ The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit int
 
 You must write an algorithm that runs in O(n) time and without using the division operation.
 
-
-***
+---
 
 # EXAMPLES
-#### Example 1.
 
+#### Example 1.
 
 Input: nums = [1,2,3,4]
 Output: [24,12,8,6]
 
-
-
 #### Example 2.
-
 
 Input: nums = [-1,1,0,-3,3]
 Output: [0,0,9,0,0]
--- src/leetcode/leetcode75/product-of-array-except-self/solution.js
+++ src/leetcode/leetcode75/product-of-array-except-self/solution.js
@@ -1,29 +1,46 @@
 /**
  * https://leetcode.com/problems/product-of-array-except-self/?envType=study-plan-v2&envId=leetcode-75
+ * 24-09-23 - X
  * 24-09-05 - O
  * 24-02-05 - X
  * 23-11-14 - O
  */
 
 /**
- * 24-09-05 - O
+ * 24-09-23 - X
  */
-var productExceptSelf = function(nums) {
-  let arr=[]
-  let max = nums.reduce((acc,cur)=>acc*=cur,1);
-  for(let i =0;i<nums.length;i++){
-      let sum;
-      if(nums[i]===0){
-          sum = nums.reduce((acc,cur,index)=>acc*=index!==i?cur:1,1);
-
-      }else{
-          sum = Math.floor(max/nums[i]);
-      }
-      arr.push(sum);
+var productExceptSelf = function (nums) {
+  let arr = [];
+  let max = nums.reduce((acc, cur) => (acc *= cur), 1);
+  for (let i = 0; i < nums.length; i++) {
+    let sum;
+    if (nums[i] === 0) {
+      sum = nums.reduce((acc, cur, index) => (acc *= index !== i ? cur : 1), 1);
+    } else {
+      sum = Math.floor(max / nums[i]);
+    }
+    arr.push(sum);
   }
   return arr;
 };
 
+/**
+ * 24-09-05 - O
+ */
+var productExceptSelf = function (nums) {
+  let arr = [];
+  let max = nums.reduce((acc, cur) => (acc *= cur), 1);
+  for (let i = 0; i < nums.length; i++) {
+    let sum;
+    if (nums[i] === 0) {
+      sum = nums.reduce((acc, cur, index) => (acc *= index !== i ? cur : 1), 1);
+    } else {
+      sum = Math.floor(max / nums[i]);
+    }
+    arr.push(sum);
+  }
+  return arr;
+};
 
 /**
  * 24.02.05
@@ -31,15 +48,15 @@ var productExceptSelf = function(nums) {
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function(nums) {
+var productExceptSelf = function (nums) {
   let result = new Array(nums.length).fill(1);
-  for(let i = 0 ;i < nums.length;i++){
-    for(let j = 0 ;j <result.length ;j++){
-      if(i!==j) result[j]*=nums[i]
-      if(result[j]===-0) result[j]=0
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < result.length; j++) {
+      if (i !== j) result[j] *= nums[i];
+      if (result[j] === -0) result[j] = 0;
     }
   }
-    return result
+  return result;
 };
 
 /**
@@ -47,64 +64,62 @@ var productExceptSelf = function(nums) {
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function(nums) {
+var productExceptSelf = function (nums) {
   // Set up an empty array as our result
-  const result = []
-  
+  const result = [];
+
   // Initialize a prefix tracker at 1
-  let prefix = 1
-  
+  let prefix = 1;
+
   // Loop through the input array - for each position,
   // the result array should equal the prefix tracker.
-  
+
   // Then, update the prefix tracker to be the product of itself,
   // multiplied by the input value at the position.
-  for (let i=0; i<nums.length; i++) {
-      result[i] = prefix
-      prefix *= nums[i]
-  }
-  
-  // Initialize a suffix tracker at 1
-  let suffix = 1
-  
-  // Loop backwards through the array.
-  // For each iteration, set the result array to be 
-  // the product of itself multiplied by the suffix tracker.
-  
-  // Then, update the suffix tracker to be the product of itself,
-  // multiplied by the input value at that position.
-  for (let i=nums.length - 1; i>=0; i--) {
-      result[i] *= suffix
-      suffix *= nums[i]
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = prefix;
+    prefix *= nums[i];
   }
 
-  return result
+  // Initialize a suffix tracker at 1
+  let suffix = 1;
+
+  // Loop backwards through the array.
+  // For each iteration, set the result array to be
+  // the product of itself multiplied by the suffix tracker.
+
+  // Then, update the suffix tracker to be the product of itself,
+  // multiplied by the input value at that position.
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= suffix;
+    suffix *= nums[i];
+  }
+
+  return result;
 };
 
 // console.log(productExceptSelf([1,2,3,4]))
-console.log(productExceptSelf([-1,1,0,-3,3]))
-
+console.log(productExceptSelf([-1, 1, 0, -3, 3]));
 
 /**
  * 내 코드
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function(nums) {
+var productExceptSelf = function (nums) {
   let newArray = [];
-  for (var i = 0; i < nums.length; i++){
-    let newNum = nums[i]===0?1:1
-    for(var j = 0; j < nums.length; j++){
-      if(i!==j){
-        console.log(nums[j])
-        newNum*=nums[j]
+  for (var i = 0; i < nums.length; i++) {
+    let newNum = nums[i] === 0 ? 1 : 1;
+    for (var j = 0; j < nums.length; j++) {
+      if (i !== j) {
+        console.log(nums[j]);
+        newNum *= nums[j];
       }
     }
-    console.log(newNum)
-  newArray.push(newNum)
-
-}
-return newArray
+    console.log(newNum);
+    newArray.push(newNum);
+  }
+  return newArray;
 };
 
 /**
@@ -120,7 +135,7 @@ return newArray
 //       res[i] = productPlaceholder;
 //       productPlaceholder *= nums[i];
 //     }
-    
+
 //     productPlaceholder = 1;
 //     for ( let j = nums.length - 1; j >= 0; j-- ) {
 //       res[j] *= productPlaceholder;
