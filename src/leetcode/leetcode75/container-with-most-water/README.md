de8a9c32e36af51cfe207a9f540d72153e5900f3
Lawen-s <bg920611@gmail.com>
2024-10-03 20:54:19 +0900

    X: container-with-most-water

---
+++ src/leetcode/leetcode75/container-with-most-water/README.md
@@ -0,0 +1,38 @@
+# 11.Container-With-Most-Water
+
+# 링크
+
+[leetcode.11](https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=leetcode-75)
+
+# 날짜 (JS)
+
+- 24-10-03 - X
+- 24-02-20 - X
+- 23-11-26 - X
+
+# 문제 설명
+
+#### 원문
+
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+# EXAMPLES
+
+#### Example 1.
+
+![Alt text](https://s3-lc-upload.s3.amazonaws.com/uploads/2018/07/17/question_11.jpg)
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+#### Example 2.
+
+Input: height = [1,1]
+Output: 1
--- src/leetcode/leetcode75/container-with-most-water/x.js
+++ src/leetcode/leetcode75/container-with-most-water/x.js
@@ -1,77 +1,101 @@
 /**
  * https://leetcode.com/problems/container-with-most-water/submissions/1099456924/?envType=study-plan-v2&envId=leetcode-75
- * 23-11-26 - X
+ * 24-10-03 - X
  * 24-02-20 - X
+ * 23-11-26 - X
  */
+
+/**
+ * 24-10-03 - X
+ */
+var maxArea = function (height) {
+  let maxArea = 0;
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+    maxArea = Math.max(
+      maxArea,
+      (right - left) * Math.min(height[left], height[right])
+    );
+
+    if (height[left] <= height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return maxArea;
+};
 
 /**
  * 24-02-20 - X (내가 푼 방식-타임에러 발생)
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
-    let area = 0;
-      for(let i =0; i < height.length; i++) {
-        for(let j = i; j < height.length; j++){
-          let small = height[i]>height[j]?height[j]:height[i];
-          let row = small*(j-i);
-          area = area >= row ?area:row
-        }
-      }
-      return area
+var maxArea = function (height) {
+  let area = 0;
+  for (let i = 0; i < height.length; i++) {
+    for (let j = i; j < height.length; j++) {
+      let small = height[i] > height[j] ? height[j] : height[i];
+      let row = small * (j - i);
+      area = area >= row ? area : row;
+    }
+  }
+  return area;
 };
 /**
  * 풀이 참고
- * @param {*} height 
- * @returns 
+ * @param {*} height
+ * @returns
  */
-var maxArea = function(height) {
-let result = 0,
-      left = 0,
-      right = height.length - 1;
-  
-    while (left < right) {
-      let smallestSide = Math.min(height[left], height[right]);
-      let area = (right - left) * smallestSide;
-  
-      if (area > result) result = area;
-  
-      if (height[left] < height[right]) left++;
-      else right--;
-    }
-  
-    return result;
+var maxArea = function (height) {
+  let result = 0,
+    left = 0,
+    right = height.length - 1;
+
+  while (left < right) {
+    let smallestSide = Math.min(height[left], height[right]);
+    let area = (right - left) * smallestSide;
+
+    if (area > result) result = area;
+
+    if (height[left] < height[right]) left++;
+    else right--;
   }
+
+  return result;
+};
 /**
  * 내가 푼 방식(timeout 발생)
  * timeout을 없애려면 while 문으로 양끝에서 조회하는 방식으로 가야함
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
+var maxArea = function (height) {
   let max = 0;
-  let maxNum = Math.max(...height)
-  console.log(maxNum)
-  for(let i = 0; i < height.length; i++) {
-    for(let j = 0; j < height.length; j++) {
-      if(height[i]>height[j]){
-        let some = i>j?i-j:j-i
-        let check=some*height[j]
-        if (max<check){
-          max=check
+  let maxNum = Math.max(...height);
+  console.log(maxNum);
+  for (let i = 0; i < height.length; i++) {
+    for (let j = 0; j < height.length; j++) {
+      if (height[i] > height[j]) {
+        let some = i > j ? i - j : j - i;
+        let check = some * height[j];
+        if (max < check) {
+          max = check;
         }
-      }else{
-        let some = i>j?i-j:j-i
-        let check = some*height[i]
-        if (max<check){
-          max=check
+      } else {
+        let some = i > j ? i - j : j - i;
+        let check = some * height[i];
+        if (max < check) {
+          max = check;
         }
       }
     }
   }
-  return max
+  return max;
 };
-
 
 /**
  * 검색 후 찾은 답
@@ -80,20 +104,21 @@ var maxArea = function(height) {
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
+var maxArea = function (height) {
   let max = 0;
-    let start = 0;
-    let last = height.length - 1;
-    
-    while (last - start > 0) {
-        const standard = height[last] > height[start] ? height[start] : height[last];
-        const gap = (last - start) * standard;
-              
-        height[last] > height[start] ? start++ : last--;
-        max = max < gap ? gap : max;
-    }
-    return max;
+  let start = 0;
+  let last = height.length - 1;
+
+  while (last - start > 0) {
+    const standard =
+      height[last] > height[start] ? height[start] : height[last];
+    const gap = (last - start) * standard;
+
+    height[last] > height[start] ? start++ : last--;
+    max = max < gap ? gap : max;
+  }
+  return max;
 };
 
-console.log(maxArea([1,8,6,2,5,4,8,3,7]))
-console.log(maxArea([1,1]))
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+console.log(maxArea([1, 1]));
