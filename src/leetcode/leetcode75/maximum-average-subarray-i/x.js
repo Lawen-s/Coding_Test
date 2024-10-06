31762a4f3c959a55f4620a2f354491a3ae3be449
Lawen-s <bg920611@gmail.com>
2024-10-06 21:18:37 +0900

    X: maximum-average-subarray-i

--- src/leetcode/leetcode75/maximum-average-subarray-i/x.js
+++ src/leetcode/leetcode75/maximum-average-subarray-i/x.js
@@ -1,15 +1,33 @@
 /**
  * https://leetcode.com/problems/maximum-average-subarray-i/solutions/2644131/js-with-explanation-easy-to-understand/?envType=study-plan-v2&envId=leetcode-75
+ * 2024-10-06 - X
  * 2024-02-21 - X
  */
 
+/**
+ * 2024-10-06 - X
+ */
+var findMaxAverage = function (nums, k) {
+  let sum = 0;
+  for (let i = 0; i < k; i++) {
+    sum += nums[i];
+  }
+
+  let max = sum;
+  for (let i = k; i < nums.length; i++) {
+    sum = sum - nums[i - k] + nums[i];
+    max = Math.max(max, sum);
+  }
+
+  return max / k;
+};
 /**
  * 2024-02-21 - 풀이를 보고 이해
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
-var findMaxAverage = function(nums, k) {
+var findMaxAverage = function (nums, k) {
   let left = 0;
   let largestTotal = 0;
   let currentTotal = 0;
@@ -34,5 +52,5 @@ var findMaxAverage = function(nums, k) {
   return largestTotal / k;
 };
 
-console.log(findMaxAverage([1,12,-5,-6,50,3],4))
+console.log(findMaxAverage([1, 12, -5, -6, 50, 3], 4));
 // console.log(findMaxAverage([5], 1))
