2f103c1048603448da5fb5cd8af6c72d77a6b8ea
Lawen-s <bg920611@gmail.com>
2024-10-09 16:55:38 +0900

    O: move-zeroes

--- src/leetcode/leetcode75/move-zeroes/solution.js
+++ src/leetcode/leetcode75/move-zeroes/solution.js
@@ -1,14 +1,32 @@
 /**
  * https://leetcode.com/problems/move-zeroes/submissions/1099354251/?envType=study-plan-v2&envId=leetcode-75
- *  2024-10-04 - X
- *  2024-09-25 - O
- *  2024-09-24 - O
- *  2024-02-15 - O
- *  2023-11-15 - O
+ *  24-10-09 - O
+ *  24-10-04 - X
+ *  24-09-25 - O
+ *  24-09-24 - O
+ *  24-02-15 - O
+ *  23-11-15 - O
  */
 
 /**
- *  2024-10-04 - X
+ *  24-10-09 - O
+ */
+var moveZeroes = function (nums) {
+  let start = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      let temp = nums[i];
+      nums[i] = nums[i - start];
+      nums[i - start] = temp;
+    } else {
+      start++;
+    }
+  }
+  return nums;
+};
+
+/**
+ *  24-10-04 - X
  */
 var moveZeroes = function (nums) {
   let left = 0;
