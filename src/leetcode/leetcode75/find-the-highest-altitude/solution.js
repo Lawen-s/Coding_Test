641565a0312fc11eae5669dc155e9f087d347b7e
Lawen-s <bg920611@gmail.com>
2024-10-14 07:01:54 +0900

    O: find-the-highest-altitude

--- src/leetcode/leetcode75/find-the-highest-altitude/solution.js
+++ src/leetcode/leetcode75/find-the-highest-altitude/solution.js
@@ -1,6 +1,20 @@
 /**
  * https://leetcode.com/problems/find-the-highest-altitude/submissions/1192733876/?envType=study-plan-v2&envId=leetcode-75
+ * 24-10-14 - O
+ * 24-03-03 - O
  */
+
+/**
+ * 24-10-14 - O
+ */
+var largestAltitude = function (gain) {
+  let height = [0];
+  gain.reduce((a, c) => {
+    height.push(c + a);
+    return c + a;
+  }, height[0]);
+  return Math.max(...height);
+};
 
 /**
  *  2024-03-03 - O
@@ -8,29 +22,32 @@
  * @param {number[]} gain
  * @return {number}
  */
-var largestAltitude = function(gain) {
-  let maxNum = 0
-    gain.reduce((acc,cur)=>{
-      console.log(acc+cur)
-    if(acc+cur>maxNum) {return maxNum=acc+cur}
-    else{return acc+cur}
-  },0)
-  return maxNum
+var largestAltitude = function (gain) {
+  let maxNum = 0;
+  gain.reduce((acc, cur) => {
+    console.log(acc + cur);
+    if (acc + cur > maxNum) {
+      return (maxNum = acc + cur);
+    } else {
+      return acc + cur;
+    }
+  }, 0);
+  return maxNum;
 };
 
 /** 풀이 확인(효율 좋음)
  * @param {number[]} gain
  * @return {number}
  */
-var largestAltitude = function(gain) {
-  var highest =0;
+var largestAltitude = function (gain) {
+  var highest = 0;
   var curAtt = 0;
   for (var att of gain) {
-      curAtt = att+curAtt;
-      highest = Math.max(highest,curAtt);
+    curAtt = att + curAtt;
+    highest = Math.max(highest, curAtt);
   }
   return highest;
 };
 
-console.log(largestAltitude([-5,1,5,0,-7]))
+console.log(largestAltitude([-5, 1, 5, 0, -7]));
 // console.log(largestAltitude([-4,-3,-2,-1,4,3,2]))
