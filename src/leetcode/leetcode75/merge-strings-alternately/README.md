2485f0041aa327b03ad422eddd1c7f56e26ca82d
Lawen-s <bg920611@gmail.com>
2024-10-19 17:00:30 +0900

    O: merge-strings-alternately

--- src/leetcode/leetcode75/merge-strings-alternately/README.md
+++ src/leetcode/leetcode75/merge-strings-alternately/README.md
@@ -1,56 +1,52 @@
 # 1768.Merge-Strings-Alternately
 
 # 링크
+
 [leetcode.1768](https://leetcode.com/problems/merge-strings-alternately/?envType=study-plan-v2&envId=leetcode-75)
 
 # 날짜(JS, PY)
 
-* 24-08-20 - O , O
-* 24-08-15 - O , O
-* 24-08-13 - O
-* 23-11-12 - O
+- 24-10-19 - O
+- 24-08-20 - O , O
+- 24-08-15 - O , O
+- 24-08-13 - O
+- 23-11-12 - O
 
 # 문제 설명
-#### 원문
 
+#### 원문
 
 You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
 
 Return the merged string.
 
-
-***
+---
 
 # EXAMPLES
-#### Example 1.
 
+#### Example 1.
 
 Input: word1 = "abc", word2 = "pqr"
 Output: "apbqcr"
 Explanation: The merged string will be merged as so:
-word1:  a   b   c
-word2:    p   q   r
+word1: a b c
+word2: p q r
 merged: a p b q c r
 
-
-
 #### Example 2.
-
 
 Input: word1 = "ab", word2 = "pqrs"
 Output: "apbqrs"
 Explanation: Notice that as word2 is longer, "rs" is appended to the end.
-word1:  a   b 
-word2:    p   q   r   s
-merged: a p b q   r   s
-
+word1: a b
+word2: p q r s
+merged: a p b q r s
 
 #### Example 3.
-
 
 Input: word1 = "abcd", word2 = "pq"
 Output: "apbqcd"
 Explanation: Notice that as word1 is longer, "cd" is appended to the end.
-word1:  a   b   c   d
-word2:    p   q 
-merged: a p b q c   d
+word1: a b c d
+word2: p q
+merged: a p b q c d
--- src/leetcode/leetcode75/merge-strings-alternately/solution.js
+++ src/leetcode/leetcode75/merge-strings-alternately/solution.js
@@ -1,5 +1,6 @@
 /**
  * https://leetcode.com/problems/merge-strings-alternately/description/?envType=study-plan-v2&envId=leetcode-75
+ * 24-10-19 - O
  * 24-08-20 - O
  * 24-08-15 - O
  * 24-08-13 - O
@@ -7,45 +8,62 @@
  */
 
 /**
+ * 24-10-19 - O
+ */
+var mergeAlternately = function (word1, word2) {
+  let result = "";
+  let max = Math.max(word1.length, word2.length);
+  for (let i = 0; i < max; i++) {
+    if (word1[i]) {
+      result += word1[i];
+    }
+    if (word2[i]) {
+      result += word2[i];
+    }
+  }
+  return result;
+};
+
+/**
  * 24-08-20 - O
  */
-var mergeAlternately = function(word1, word2) {
-    let max = Math.max(word1.length, word2.length);
-    let result = '';
-    for(let i =0;i<max;i++){
-        let word1Str = word1[i]?word1[i]:'';
-        let word2Str = word2[i]?word2[i]:'';
-        result+=word1Str+word2Str;
-    }
-    return result;
+var mergeAlternately = function (word1, word2) {
+  let max = Math.max(word1.length, word2.length);
+  let result = "";
+  for (let i = 0; i < max; i++) {
+    let word1Str = word1[i] ? word1[i] : "";
+    let word2Str = word2[i] ? word2[i] : "";
+    result += word1Str + word2Str;
+  }
+  return result;
 };
 
 /**
  * 24-08-15 - O
  */
-var mergeAlternately = function(word1, word2) {
-    let max = Math.max(word1.length,word2.length);
-    let result = '';
-    for(let i =0;i<max;i++){
-        let word1Index = word1[i]?word1[i]:'';
-        let word2Index = word2[i]?word2[i]:'';
-        result+=word1Index+word2Index;
-    }
-    return result;
+var mergeAlternately = function (word1, word2) {
+  let max = Math.max(word1.length, word2.length);
+  let result = "";
+  for (let i = 0; i < max; i++) {
+    let word1Index = word1[i] ? word1[i] : "";
+    let word2Index = word2[i] ? word2[i] : "";
+    result += word1Index + word2Index;
+  }
+  return result;
 };
 
 /**
  * 24-08-13 - O
  */
-var mergeAlternately = function(word1, word2) {
-    let max = Math.max(word1.length,word2.length);
-    let newstr = '';
-    for(let i =0;i<max;i++){
-        let word1Str = word1[i]?word1[i]:'';
-        let word2Str = word2[i]?word2[i]:'';
-        newstr+=word1Str+word2Str;
-    }
-    return newstr;
+var mergeAlternately = function (word1, word2) {
+  let max = Math.max(word1.length, word2.length);
+  let newstr = "";
+  for (let i = 0; i < max; i++) {
+    let word1Str = word1[i] ? word1[i] : "";
+    let word2Str = word2[i] ? word2[i] : "";
+    newstr += word1Str + word2Str;
+  }
+  return newstr;
 };
 
 /**
@@ -53,20 +71,20 @@ var mergeAlternately = function(word1, word2) {
  * @param {string} word2
  * @return {string}
  */
-var mergeAlternately = function(word1, word2) {
+var mergeAlternately = function (word1, word2) {
   let oneLength = word1.length;
   let twoLength = word2.length;
-  let newWord = '';
-  let wordLength = oneLength>=twoLength?oneLength:twoLength;
-  for(let i=0;i<=wordLength;i++){
-      if(i<oneLength){
-          newWord+=word1[i];
-      }
-      if(i<twoLength){
-          newWord+=word2[i];
-      }
+  let newWord = "";
+  let wordLength = oneLength >= twoLength ? oneLength : twoLength;
+  for (let i = 0; i <= wordLength; i++) {
+    if (i < oneLength) {
+      newWord += word1[i];
+    }
+    if (i < twoLength) {
+      newWord += word2[i];
+    }
   }
-  return newWord
+  return newWord;
 };
 
 /**
@@ -75,13 +93,13 @@ var mergeAlternately = function(word1, word2) {
  * @param {string} word2
  * @return {string}
  */
-var mergeAlternately = function(word1, word2) {
-    let num = word1.length>= word2.length? word1.length: word2.length;
-    let merge = ''
-    for(let i=0;i<num;i++){
-        w1=word1[i]?word1[i]:''
-        w2=word2[i]?word2[i]:''
-        merge += w1+w2
-    }
-    return merge
+var mergeAlternately = function (word1, word2) {
+  let num = word1.length >= word2.length ? word1.length : word2.length;
+  let merge = "";
+  for (let i = 0; i < num; i++) {
+    w1 = word1[i] ? word1[i] : "";
+    w2 = word2[i] ? word2[i] : "";
+    merge += w1 + w2;
+  }
+  return merge;
 };
