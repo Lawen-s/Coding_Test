0afd8ac8e0a02f89a8e5d4fe2fa49e848fba83c6
Lawen-s <bg920611@gmail.com>
2024-10-19 16:43:15 +0900

    O: unique-number-of-occurrences

---
+++ src/leetcode/leetcode75/unique-number-of-occurrences/README.md
@@ -0,0 +1,35 @@
+# 1207.Unique-Number-Of-Occurrences
+
+# 링크
+
+[leetcode.1207](https://leetcode.com/problems/unique-number-of-occurrences/?envType=study-plan-v2&envId=leetcode-75)
+
+# 날짜
+
+- 24-10-19 - O
+- 24-03-10 - O
+- 23-11-23 - O
+
+# 문제 설명
+
+#### 원문
+
+Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+# EXAMPLES
+
+#### Example 1.
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+
+#### Example 2.
+
+Input: arr = [1,2]
+Output: false
+
+#### Example 3.
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
--- src/leetcode/leetcode75/unique-number-of-occurrences/solution.js
+++ src/leetcode/leetcode75/unique-number-of-occurrences/solution.js
@@ -1,31 +1,59 @@
 /**
  * https://leetcode.com/problems/unique-number-of-occurrences/submissions/1104840456/?envType=study-plan-v2&envId=leetcode-75
+ * 24-10-19 - O
  * 24-03-10 - O
  * 23-11-23 - O
  */
+
+/**
+ * 24-10-19 - O
+ */
+var uniqueOccurrences = function (arr) {
+  arr.sort((a, b) => a - b);
+  let result = [];
+  let num = arr[0];
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (num !== arr[i]) {
+      if (result.includes(count)) {
+        return false;
+      }
+      result.push(count);
+      count = 1;
+      num = arr[i];
+    } else {
+      count++;
+    }
+  }
+  if (result.includes(count)) {
+    return false;
+  }
+  return true;
+};
 
 /**
  * 24-03-10 - O - 내가 푼 코드
  * @param {number[]} arr
  * @return {boolean}
  */
-var uniqueOccurrences = function(arr) {
+var uniqueOccurrences = function (arr) {
   let map1 = new Map();
-  for(let i = 0; i < arr.length; i++) {
-    if(map1.has(arr[i])){
-      map1.set(arr[i],map1.get(arr[i])+1)
+  for (let i = 0; i < arr.length; i++) {
+    if (map1.has(arr[i])) {
+      map1.set(arr[i], map1.get(arr[i]) + 1);
+    } else {
+      map1.set(arr[i], 1);
     }
-    else { map1.set(arr[i],1)}
   }
-  let newArr = [...map1.values()]
-  let check = newArr.map((x,i)=>{
-      if(newArr.indexOf(x)===i){
-        return true
-      }else{
-        return false
-      }
-    })
-    return check.includes(false)?false:true
+  let newArr = [...map1.values()];
+  let check = newArr.map((x, i) => {
+    if (newArr.indexOf(x) === i) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return check.includes(false) ? false : true;
 };
 
 /**
