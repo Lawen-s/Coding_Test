0bde71ed81cecea49d948ba741adfc688f1c35bf
Lawen-s <bg920611@gmail.com>
2024-10-29 23:00:48 +0900

    X: decode-string

--- src/leetcode/leetcode75/decode-string/x.js
+++ src/leetcode/leetcode75/decode-string/x.js
@@ -1,9 +1,38 @@
 /**
  * https://leetcode.com/problems/decode-string/submissions/1108192700/?envType=study-plan-v2&envId=leetcode-75
+ * 24-10-29 - X
  * 24-10-25 - X
  * 24-03-19 - X
  * 23-11-28 - X
  */
+
+/**
+ * 24-10-29 - X
+ */
+var decodeString = function (s) {
+  let stack = [];
+  let currStr = "";
+  let currNum = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "[") {
+      stack.push(currStr);
+      stack.push(currNum);
+      currStr = "";
+      currNum = 0;
+    } else if (s[i] === "]") {
+      let prevNum = stack.pop();
+      let prevStr = stack.pop();
+      currStr = prevStr + currStr.repeat(prevNum);
+    } else if (s[i] >= "0" && s[i] <= "9") {
+      currNum = currNum * 10 + Number(s[i]);
+    } else {
+      currStr += s[i];
+    }
+  }
+
+  return currStr;
+};
 
 /**
  * 24-10-25 - X
