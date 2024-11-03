ab1d4db17ae042ee3012b1d93544a62956a5a5bf
Lawen-s <bg920611@gmail.com>
2024-11-03 23:40:20 +0900

    O: can-place-flowers

--- src/leetcode/leetcode75/can-place-flowers/README.md
+++ src/leetcode/leetcode75/can-place-flowers/README.md
@@ -6,33 +6,28 @@
 
 # 날짜 (JS)
 
-* 24-09-10 - O
-* 24-09-09 - O
-* 24-08-27 - X
-* 24-01-31 - X
+- 24-11-03 - O
+- 24-09-10 - O
+- 24-09-09 - O
+- 24-08-27 - X
+- 24-01-31 - X
 
 # 문제 설명
 
 #### 원문
 
-
 You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
 
 Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
-
 
 # EXAMPLES
 
 #### Example 1.
 
-
 Input: flowerbed = [1,0,0,0,1], n = 1
 Output: true
 
-
-
 #### Example 2.
-
 
 Input: flowerbed = [1,0,0,0,1], n = 2
 Output: false
--- src/leetcode/leetcode75/can-place-flowers/x.js
+++ src/leetcode/leetcode75/can-place-flowers/x.js
@@ -1,5 +1,6 @@
 /**
  * 문제: https://leetcode.com/problems/can-place-flowers/?envType=study-plan-v2&envId=leetcode-75
+ * 24-11-03 - O
  * 24-09-10 - O
  * 24-09-09 - O
  * 24-08-27 - X
@@ -7,70 +8,107 @@
  */
 
 /**
- * 24-09-10 - O 
+ * 24-11-03 - O
  */
-var canPlaceFlowers = function(flowerbed, n) {
-  for(let i=0;i<flowerbed.length;i++){
-      if(n===0){
-          return true;
+var canPlaceFlowers = function (flowerbed, n) {
+  if ((flowerbed.length === 1 && flowerbed[0] === 0) || n === 0) {
+    return true;
+  }
+  for (let i = 0; i < flowerbed.length; i++) {
+    let f = flowerbed[i - 1];
+    let s = flowerbed[i];
+    let t = flowerbed[i + 1];
+    if (s === 0) {
+      if (f === 0 && t === 0) {
+        n--;
+        flowerbed[i] = 1;
       }
-      let pre = i-1<0?0:flowerbed[i-1];
-      let mid = flowerbed[i];
-      let next = i+1>=flowerbed.length?0:flowerbed[i+1];
-      if(pre+mid+next===0){
-          flowerbed[i]=1;
-          n--;
+      if (i === 0 && t === 0) {
+        n--;
+        flowerbed[i] = 1;
       }
-  }   
-  return n===0?true:false;
+      if (i === flowerbed.length - 1 && f === 0) {
+        n--;
+        flowerbed[i] = 1;
+      }
+    }
+    if (n === 0) {
+      return true;
+    }
+  }
+  return false;
+};
+
+/**
+ * 24-09-10 - O
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (n === 0) {
+      return true;
+    }
+    let pre = i - 1 < 0 ? 0 : flowerbed[i - 1];
+    let mid = flowerbed[i];
+    let next = i + 1 >= flowerbed.length ? 0 : flowerbed[i + 1];
+    if (pre + mid + next === 0) {
+      flowerbed[i] = 1;
+      n--;
+    }
+  }
+  return n === 0 ? true : false;
 };
 
 /**
  * 24-09-09 - O
  */
-var canPlaceFlowers = function(flowerbed, n) {
-  if(n===0){
-          return true;
-      }
-
-  for(let i =0;i<flowerbed.length;i++){
-      let pre = i-1<0?0:flowerbed[i-1];
-      let next = i+1>=flowerbed.length?0:flowerbed[i+1];
-      if(pre+flowerbed[i]+next===0){
-          flowerbed[i]=1;
-          n--;
-      }
-      if(n===0){
-          return true;
-      }
+var canPlaceFlowers = function (flowerbed, n) {
+  if (n === 0) {
+    return true;
   }
-  return n===0?true:false;
+
+  for (let i = 0; i < flowerbed.length; i++) {
+    let pre = i - 1 < 0 ? 0 : flowerbed[i - 1];
+    let next = i + 1 >= flowerbed.length ? 0 : flowerbed[i + 1];
+    if (pre + flowerbed[i] + next === 0) {
+      flowerbed[i] = 1;
+      n--;
+    }
+    if (n === 0) {
+      return true;
+    }
+  }
+  return n === 0 ? true : false;
 };
 
 /**
- * 24-08-27 - X 
+ * 24-08-27 - X
  */
-var canPlaceFlowers = function(flowerbed, n) {
+var canPlaceFlowers = function (flowerbed, n) {
   let count = 0;
   let pre = -1;
   let next = 1;
 
   for (let i = 0; i < flowerbed.length; i++, pre++, next++) {
-      if ((getValue(flowerbed, pre) + getValue(flowerbed, i) + getValue(flowerbed, next)) == 0) {
-          flowerbed[i] = 1;
-          count++;
-      }
+    if (
+      getValue(flowerbed, pre) +
+        getValue(flowerbed, i) +
+        getValue(flowerbed, next) ==
+      0
+    ) {
+      flowerbed[i] = 1;
+      count++;
+    }
   }
 
   return count >= n;
 };
 
-var getValue = function(flowerbed, i) {
+var getValue = function (flowerbed, i) {
   if (i < 0 || i >= flowerbed.length) {
-      return 0;
+    return 0;
   }
   return flowerbed[i];
-}
+};
 
 /**
  * 풀이 확인 (24.01.31)
@@ -78,20 +116,19 @@ var getValue = function(flowerbed, i) {
  * @param {number} n
  * @return {boolean}
  */
-var canPlaceFlowers = function(flowerbed, n) {
-    for (let i = 0; i < flowerbed.length && n !== 0; i++) {
-      if (
-        flowerbed[i] === 0 &&
-        flowerbed[i - 1] !== 1 &&
-        flowerbed[i + 1] !== 1
-      ) {
-        n--;
-        i++;
-      }
+var canPlaceFlowers = function (flowerbed, n) {
+  for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+    if (
+      flowerbed[i] === 0 &&
+      flowerbed[i - 1] !== 1 &&
+      flowerbed[i + 1] !== 1
+    ) {
+      n--;
+      i++;
     }
-    return n === 0;
+  }
+  return n === 0;
 };
-
 
 /**
  * 24.01.31
@@ -100,60 +137,56 @@ var canPlaceFlowers = function(flowerbed, n) {
  * @param {number} n
  * @return {boolean}
  */
-var canPlaceFlowers = function(flowerbed, n) {
-    let before =flowerbed[0] ;
-    let cur = flowerbed[1];
-    if(n===0||(before===0&&n===1)){
-        return true
+var canPlaceFlowers = function (flowerbed, n) {
+  let before = flowerbed[0];
+  let cur = flowerbed[1];
+  if (n === 0 || (before === 0 && n === 1)) {
+    return true;
+  }
+  for (let i = 2; i < flowerbed.length; i++) {
+    if (i === 2 && before === 0 && cur === 0) {
+      n -= 1;
+      flowerbed[i - 2] = 1;
     }
-    for(let i =2;i<flowerbed.length;i++){
-        if(i===2&&before ===0&&cur===0){
-          n-=1
-          flowerbed[i-2]=1;
-        }
-        if(i===flowerbed.length-1&&flowerbed[i]===0&&cur===0){
-            n-=1
-            flowerbed[i]=1
-        }
-        if(before ===0&&cur===0&&flowerbed[i]===0){
-          n-=1
-          flowerbed[i-1]=1;
-        }
-        if(n===0){
-          return true
-        }
-        before=flowerbed[i-1]
-        cur=flowerbed[i]
+    if (i === flowerbed.length - 1 && flowerbed[i] === 0 && cur === 0) {
+      n -= 1;
+      flowerbed[i] = 1;
     }
-    return false
-  
-  };
-
+    if (before === 0 && cur === 0 && flowerbed[i] === 0) {
+      n -= 1;
+      flowerbed[i - 1] = 1;
+    }
+    if (n === 0) {
+      return true;
+    }
+    before = flowerbed[i - 1];
+    cur = flowerbed[i];
+  }
+  return false;
+};
 
 /**
  * https://leetcode.com/problems/can-place-flowers/?source=submission-noac
  */
 
-var canPlaceFlowers = function(flowerbed, n) {
-  if(n ==0)return true;
+var canPlaceFlowers = function (flowerbed, n) {
+  if (n == 0) return true;
   let len = flowerbed.length;
   let plantCnt = 0;
 
   let prev = 0;
   for (let i = 0; i < len; i++) {
-      let next = i == len-1? 0 : flowerbed[i+1];
-      if(prev == 0 && next ==0 && flowerbed[i] ==0){
-          flowerbed[i] = 1;
-          if(++plantCnt == n){ 
-              return true;
-          }
+    let next = i == len - 1 ? 0 : flowerbed[i + 1];
+    if (prev == 0 && next == 0 && flowerbed[i] == 0) {
+      flowerbed[i] = 1;
+      if (++plantCnt == n) {
+        return true;
       }
-      prev = flowerbed[i];
+    }
+    prev = flowerbed[i];
   }
   return false;
 };
-
-
 
 /**
  * @param {number[]} flowerbed
@@ -162,9 +195,7 @@ var canPlaceFlowers = function(flowerbed, n) {
  */
 // var canPlaceFlowers = function (flowerbed, n) {
 //   let counter = 0
-  
-  
-  
+
 //   for (let i = 0; i < flowerbed.length; i++) {
 //       let prev = flowerbed[i - 1]
 //       let current = flowerbed[i]
