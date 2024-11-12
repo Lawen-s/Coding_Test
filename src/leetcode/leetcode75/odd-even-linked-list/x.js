b65e74dddbaa961dfcfb25d4d56f8c95336f3f73
Lawen-s <bg920611@gmail.com>
2024-11-12 21:56:21 +0900

    X: odd-even-linked-list

--- src/leetcode/leetcode75/odd-even-linked-list/x.js
+++ src/leetcode/leetcode75/odd-even-linked-list/x.js
@@ -1,27 +1,47 @@
 /**
  * https://leetcode.com/problems/odd-even-linked-list/submissions/1141525550/?envType=study-plan-v2&envId=leetcode-75
+ * 24-11-12 - X
  * 24-03-24 - X
  * 24-01-09 - X
  */
 
 /**
- * 24-03-24 - X
- * @param {*} head 
- * @returns 
+ * 24-11-12 - X
  */
-var oddEvenList = function(head) {
-  if(head != null){
-        
-    let odd = head, even = head.next, evenHead = even;
-    while(even && even.next != null){
-        odd.next = even.next;
+var oddEvenList = function (head) {
+  if (!head) return head;
+
+  var odd = head;
+  var even = head.next;
+  while (odd.next && odd.next.next) {
+    var tmp = odd.next;
+    odd.next = odd.next.next;
     odd = odd.next;
-    even.next = odd.next;
-    even = even.next;
+    tmp.next = odd.next;
+  }
+  odd.next = even;
+  return head;
+};
+
+/**
+ * 24-03-24 - X
+ * @param {*} head
+ * @returns
+ */
+var oddEvenList = function (head) {
+  if (head != null) {
+    let odd = head,
+      even = head.next,
+      evenHead = even;
+    while (even && even.next != null) {
+      odd.next = even.next;
+      odd = odd.next;
+      even.next = odd.next;
+      even = even.next;
     }
     odd.next = evenHead;
-}
-return head;
+  }
+  return head;
 };
 
 /**
@@ -36,21 +56,20 @@ return head;
  * @param {ListNode} head
  * @return {ListNode}
  */
-var oddEvenList = function(head) {
+var oddEvenList = function (head) {
   if (!head) return head;
 
-var odd = head;
-var even = head.next;
-while (odd.next && odd.next.next) {
-  var tmp = odd.next;
-  odd.next = odd.next.next;
-  odd = odd.next;
-  tmp.next = odd.next;
-}
-odd.next = even;
-return head;
+  var odd = head;
+  var even = head.next;
+  while (odd.next && odd.next.next) {
+    var tmp = odd.next;
+    odd.next = odd.next.next;
+    odd = odd.next;
+    tmp.next = odd.next;
+  }
+  odd.next = even;
+  return head;
 };
-
 
 /**
  * 효율이 좋은 코드
@@ -64,21 +83,21 @@ return head;
  * @param {ListNode} head
  * @return {ListNode}
  */
-var oddEvenList = function(head) {
+var oddEvenList = function (head) {
   if (!head?.next) {
-      return head;
+    return head;
   }
   let odd = head;
-  let even = head.next
+  let even = head.next;
   let evenHead = even;
 
   while (even && even.next) {
-      odd.next = even.next;
-      odd = odd.next;
-      even.next = odd.next;
-      even = even.next;
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
   }
-  
+
   odd.next = evenHead;
 
   return head;
