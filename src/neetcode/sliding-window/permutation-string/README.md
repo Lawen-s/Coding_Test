650493d788be0c56055d78e41a4cc3a909982c6c
Lawen-s <bg920611@gmail.com>
2025-03-04 22:46:35 +0900

    X: permutation-string

---
+++ src/neetcode/sliding-window/permutation-string/README.md
@@ -0,0 +1,41 @@
+# permutation-string
+
+# 링크
+
+[permutation-string](https://neetcode.io/problems/permutation-string)
+
+# 날짜
+
+- 25-03-04 - X
+
+# 문제 설명
+
+#### 원문
+
+You are given two strings s1 and s2.
+
+Return true if s2 contains a permutation of s1, or false otherwise. That means if a permutation of s1 exists as a substring of s2, then return true.
+
+Both strings only contain lowercase letters.
+
+---
+
+# EXAMPLES
+
+#### Example 1.
+
+Input: s1 = "abc", s2 = "lecabee"
+
+Output: true
+
+Explanation: The substring "cab" is a permutation of "abc" and is present in "lecabee".
+
+#### Example 2.
+
+Input: s1 = "abc", s2 = "lecaabee"
+
+Output: false
+
+Constraints:
+
+- 1 <= s1.length, s2.length <= 1000
---
+++ src/neetcode/sliding-window/permutation-string/x.js
@@ -0,0 +1,67 @@
+// 풀이 확인
+class Solution {
+  /**
+   * @param {string} s1
+   * @param {string} s2
+   * @return {boolean}
+   */
+  checkInclusion(s1, s2) {
+    // s1은 s2보다 클 수 없음
+    if (s1.length > s2.length) {
+      return false;
+    }
+    // s1 윈도우에 나오는 알파벳을 카운팅하기 위한 변수
+    let s1Count = new Array(26).fill(0);
+    // s2 윈도우에 나오는 알파벳을 count
+    let s2Count = new Array(26).fill(0);
+    for (let i = 0; i < s1.length; i++) {
+      // s1Count에는 s1의 문자 빈도를 저장
+      s1Count[s1.charCodeAt(i) - 97]++;
+      // s2Count에는 s2에서 처음 s1.length 길이의 문자열에 대한 문자 빈도를 저장
+      s2Count[s2.charCodeAt(i) - 97]++;
+    }
+    // s1Count와 s2Count에서 같은 값(문자 개수)을 가진 인덱스 개수를 추적
+    let matches = 0;
+    for (let i = 0; i < 26; i++) {
+      // 같은 문자가 있었던 경우만 matchs++
+      // 만약 matches === 26이면 두 개의 배열이 완전히 동일
+      if (s1Count[i] === s2Count[i]) {
+        matches++;
+      }
+    }
+
+    let l = 0;
+    // 윈도우를 s1의 길이로 지정후 진행
+    // 위에서 이미 윈도우의 길이로 s2Count에 count를 진행
+    for (let r = s1.length; r < s2.length; r++) {
+      if (matches === 26) {
+        return true;
+      }
+      // s2에서 윈도우를 r로 이동한 다음 문자를 확인
+      let index = s2.charCodeAt(r) - 97;
+      // s2(r) 문자를 +1
+      s2Count[index]++;
+      // s2(r) 값이 s1(r)과 같다면 match +1
+      if (s1Count[index] === s2Count[index]) {
+        matches++;
+        // s2(r) 값이 s1(r)과 다르다면 match -1
+      } else if (s1Count[index] + 1 === s2Count[index]) {
+        matches--;
+      }
+      // s2에서 l 윈도우 이동 전 문자를 확인(윈도우는 고정 크기)
+      index = s2.charCodeAt(l) - 97;
+      // s2(l) 문자를 -1
+      s2Count[index]--;
+      // s2(l) 값이 s1(l)과 같다면 match +1
+      if (s1Count[index] === s2Count[index]) {
+        matches++;
+        // s2(l) 값이 s1(l)과 다르다면 match -1
+      } else if (s1Count[index] - 1 === s2Count[index]) {
+        matches--;
+      }
+      // l 윈도우 이동
+      l++;
+    }
+    return matches === 26;
+  }
+}
