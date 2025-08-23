bbef83b84da0ffaf6de1ff0677fc2c90460c8ec7
Lawen-s <bg920611@gmail.com>
2025-08-23 18:26:00 +0900

    O:로또의 최고순위와 최저순위

--- src/programmers/Lv1/로또의최고순위와최저순위/Solution.js
+++ src/programmers/Lv1/로또의최고순위와최저순위/Solution.js
@@ -1,18 +1,49 @@
 function solution(lottos, win_nums) {
-    let answer = [];
-    let rate = { 6: 1, 5: 2, 4: 3, 3: 4, 2: 5, 1: 6, 0: 6 };
-    let low = 0;
-    let max = 0;
-    for (let el of lottos) {
-        let lowRate = win_nums.filter((num) => el === num)
-        if (lowRate.length !== 0) {
-            low++;
-            max++;
-        }
-        if (el === 0) {
-            max++;
-        }
+  let setWinNumbs = new Set(win_nums);
+  let count = 0;
+  let zeroCount = 0;
+  lottos.map((v) => (v === 0 ? zeroCount++ : 0));
+  for (let i = 0; i < 6; i++) {
+    if (setWinNumbs.has(lottos[i])) {
+      count++;
     }
-    answer = [rate[max], rate[low]];
-    return answer;
+  }
+  return [Rate(count + zeroCount), Rate(count)];
+}
+
+function Rate(rate) {
+  switch (rate) {
+    case 6:
+      return 1;
+    case 5:
+      return 2;
+    case 4:
+      return 3;
+    case 3:
+      return 4;
+    case 2:
+      return 5;
+    default:
+      return 6;
+  }
+}
+
+//
+function solution(lottos, win_nums) {
+  let answer = [];
+  let rate = { 6: 1, 5: 2, 4: 3, 3: 4, 2: 5, 1: 6, 0: 6 };
+  let low = 0;
+  let max = 0;
+  for (let el of lottos) {
+    let lowRate = win_nums.filter((num) => el === num);
+    if (lowRate.length !== 0) {
+      low++;
+      max++;
+    }
+    if (el === 0) {
+      max++;
+    }
+  }
+  answer = [rate[max], rate[low]];
+  return answer;
 }
