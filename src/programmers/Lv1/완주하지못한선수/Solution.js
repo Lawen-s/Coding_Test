b948a37303d02cb2fec03d438066208dda41a4e9
Lawen-s <bg920611@gmail.com>
2025-08-23 17:13:49 +0900

    O: 완주하지 못한 선수

--- src/programmers/Lv1/완주하지못한선수/Solution.js
+++ src/programmers/Lv1/완주하지못한선수/Solution.js
@@ -1,46 +1,56 @@
 function solution(participant, completion) {
-    const map = new Map();
-
-    for(let i = 0; i < participant.length; i++) {
-        let a = participant[i], 
-            b = completion[i];
-
-        map.set(a, (map.get(a) || 0) + 1);
-        map.set(b, (map.get(b) || 0) - 1);
+  let sortParticipant = participant.sort();
+  let sortCompletion = completion.sort();
+  for (let i = 0; i < sortParticipant.length; i++) {
+    if (sortParticipant[i] !== sortCompletion[i]) {
+      return sortParticipant[i];
     }
+  }
+  return sortParticipant[0];
+}
 
-    for(let [k, v] of map) {
-        if(v > 0) return k;
-    }
+function solution(participant, completion) {
+  const map = new Map();
 
-    return 'nothing';
+  for (let i = 0; i < participant.length; i++) {
+    let a = participant[i],
+      b = completion[i];
+
+    map.set(a, (map.get(a) || 0) + 1);
+    map.set(b, (map.get(b) || 0) - 1);
+  }
+
+  for (let [k, v] of map) {
+    if (v > 0) return k;
+  }
+
+  return "nothing";
 }
 
 //시간초과1.
 // function solution(participant, completion) {
 //     let answer = "";
-     
+
 //     for(let i =0;i<completion.length;i++){
 //                 let a=participant.indexOf(completion[i]);
-//                 participant.splice(a,1);                    
+//                 participant.splice(a,1);
 //     }
 //     answer+=participant;
 //     return answer;
 // }
 
-
 //시간초과2.
 // function solution(participant, completion) {
 //     let answer = "";
-     
+
 //     for(let i =0;i<participant.length;i++){
 //             if(!completion.includes(participant[i])){
 //                answer+=participant[i];
 //             }else{
 //                 let a=completion.indexOf(participant[i]);
-//                 completion.splice(a,1);             
+//                 completion.splice(a,1);
 //             }
-        
+
 //     }
 //     return answer;
 // }
