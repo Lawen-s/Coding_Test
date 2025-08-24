b1e6d61b4b2edce5b17b370dfcee83f08dd211af
Lawen-s <bg920611@gmail.com>
2025-08-24 17:40:44 +0900

    O: 모의고사

--- src/programmers/Lv1/모의고사/Solution.js
+++ src/programmers/Lv1/모의고사/Solution.js
@@ -1,17 +1,35 @@
 function solution(answers) {
-    let answer = [];
-    let one = [1, 2, 3, 4, 5];
-    let two = [2, 1, 2, 3, 2, 4, 2, 5];
-    let thr = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  let f = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
+  let s = [2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5];
+  let t = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  let users = [f, s, t];
+  for (let i = 0; i < users.length; i++) {
+    users[i] = Check(answers, users[i]).length;
+  }
+  const max = Math.max(...users);
+  let result = [];
+  users.map((v, i) => (max === v ? result.push(i + 1) : null));
+  return result;
+}
 
-    let oneR = answers.filter((el, idx) => el === one[idx % one.length]).length;
-    let twoR = answers.filter((el, idx) => el === two[idx % two.length]).length;
-    let thrR = answers.filter((el, idx) => el === thr[idx % thr.length]).length;
+function Check(answers, user) {
+  return answers.filter((v, i) => v === user[i % user.length]);
+}
 
-    let maxNum = Math.max(oneR, twoR, thrR);
-    let newArr = [oneR, twoR, thrR];
-    for (let i = 0; i <= newArr.length; i++) {
-        if (maxNum === newArr[i]) answer.push(i + 1)
-    }
-    return answer;
+function solution(answers) {
+  let answer = [];
+  let one = [1, 2, 3, 4, 5];
+  let two = [2, 1, 2, 3, 2, 4, 2, 5];
+  let thr = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  let oneR = answers.filter((el, idx) => el === one[idx % one.length]).length;
+  let twoR = answers.filter((el, idx) => el === two[idx % two.length]).length;
+  let thrR = answers.filter((el, idx) => el === thr[idx % thr.length]).length;
+
+  let maxNum = Math.max(oneR, twoR, thrR);
+  let newArr = [oneR, twoR, thrR];
+  for (let i = 0; i <= newArr.length; i++) {
+    if (maxNum === newArr[i]) answer.push(i + 1);
+  }
+  return answer;
 }
