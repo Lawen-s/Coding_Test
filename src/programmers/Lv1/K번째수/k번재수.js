16634234591ca43be278d9bd21dbbf89f64e196b
Lawen-s <bg920611@gmail.com>
2025-08-24 16:52:12 +0900

    O: K번째수

--- src/programmers/Lv1/K번째수/k번재수.js
+++ src/programmers/Lv1/K번째수/k번재수.js
@@ -1,8 +1,20 @@
 function solution(array, commands) {
-    let answer = [];
-    for (let el of commands) {
-        let spread = array.slice(el[0] - 1, el[1]).sort((a, b) => a - b)[el[2] - 1]
-        answer.push(spread)
-    }
-    return answer;
+  let result = [];
+  for (let i = 0; i < commands.length; i++) {
+    let start = commands[i][0] - 1;
+    let end = commands[i][1];
+    let select = commands[i][2] - 1;
+    let sliceArr = array.slice(start, end).sort((a, b) => a - b)[select];
+    result.push(sliceArr);
+  }
+  return result;
+}
+
+function solution(array, commands) {
+  let answer = [];
+  for (let el of commands) {
+    let spread = array.slice(el[0] - 1, el[1]).sort((a, b) => a - b)[el[2] - 1];
+    answer.push(spread);
+  }
+  return answer;
 }
